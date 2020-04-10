9d5329c524b5912d6ddd6b1eb6ba36005ab2336a
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-04-10 15:59:18 +0800

    chore: lint fix

--- pages/index.vue
+++ pages/index.vue
@@ -9,11 +9,7 @@
         My gnarly Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
           Documentation
         </a>
         <a
