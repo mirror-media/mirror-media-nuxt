c716ffa0ca65916fefa9e804f02796a02a0a476f
Noah Tan <tempo0829@gmail.com>
2020-04-30 19:16:22 +0800

    feat: set specific url response header

---
+++ api/headers.js
@@ -0,0 +1,27 @@
+const noCacheRule = {
+  'cache-control': 'no-cache, no-store, max-age=0, must-revalidate',
+  pragma: 'no-cache',
+}
+
+module.exports = function (req, res, next) {
+  const hostname = req.hostname
+  if (hostname.match(/dev.mirrormedia.mg/gs)) {
+    res.set(noCacheRule)
+    return next()
+  }
+
+  if (hostname.match(/www.mirrormedia.mg/gs)) {
+    if (
+      req.url.match(
+        /^\/$|^\/(section|category|topic|externals|search|author|tag)\//gs
+      )
+    ) {
+      res.set(noCacheRule)
+    }
+    if (req.url.match(/^\/(story|app)\//gs)) {
+      res.set('cache-control', 'public, max-age=600')
+    }
+    return next()
+  }
+  next()
+}
