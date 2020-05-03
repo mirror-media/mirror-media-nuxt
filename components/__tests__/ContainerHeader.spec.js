313742dedfc162d3a0f0461c87aa4f8767de64b0
yeefun <szyeefun@gmail.com>
2020-05-03 18:02:14 +0800

    test: add ContainerHeader test

---
+++ components/__tests__/ContainerHeader.spec.js
@@ -0,0 +1,47 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import ContainerHeader from '../ContainerHeader.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('computed Vuex state', () => {
+  test('get the proper data', () => {
+    const store = new Vuex.Store({
+      state: {
+        sections: { items: [{ name: 'news' }] },
+        topics: { items: [{ name: '網紅星勢力' }] },
+        partners: { items: [{ name: 'healthnews' }] },
+      },
+    })
+    const wrapper = shallowMount(ContainerHeader, { store, localVue })
+
+    expect(wrapper.vm.sections).toEqual([{ name: 'news' }])
+    expect(wrapper.vm.topics).toEqual([{ name: '網紅星勢力' }])
+    expect(wrapper.vm.partners).toEqual([{ name: 'healthnews' }])
+  })
+
+  test('get a [] when the data is undefined', () => {
+    const store = new Vuex.Store({
+      state: {
+        sections: {},
+        topics: {},
+        partners: {},
+      },
+    })
+    const wrapper = shallowMount(ContainerHeader, { store, localVue })
+
+    expect(wrapper.vm.sections).toEqual([])
+    expect(wrapper.vm.topics).toEqual([])
+    expect(wrapper.vm.partners).toEqual([])
+  })
+})
+
+describe('markup', () => {
+  test('has a header tag', () => {
+    const store = new Vuex.Store()
+    const wrapper = shallowMount(ContainerHeader, { store, localVue })
+
+    expect(wrapper.contains('header')).toBe(true)
+  })
+})
