e38ecff2c2283a2294239a318da17e2adbc4f57d
HY Tan <tempo0829@gmail.com>
2020-05-04 10:45:20 +0800

    feat: add helmet for security issue (#25)

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 
+const helmet = require('helmet')
+
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
@@ -19,6 +21,8 @@ async function start() {
     const builder = new Builder(nuxt)
     await builder.build()
   }
+
+  app.use(helmet())
 
   // Give nuxt middleware to express
   app.use(nuxt.render)
