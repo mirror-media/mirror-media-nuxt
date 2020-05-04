855b620dfb6aae7f32090a6ed76571126d542013
yeefun <szyeefun@gmail.com>
2020-05-04 19:32:55 +0800

    refactor: change data fetching from middleware to nuxtServerInit and layout fetch method

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,14 +7,39 @@
 </template>
 
 <script>
+import { camelizeKeys } from 'humps'
+
 import ContainerHeader from '~/components/ContainerHeader.vue'
 import UIFooter from '~/components/UIFooter.vue'
 
 export default {
-  middleware: ['fetchDefaultCommonData'],
   components: {
     UIFooter,
     ContainerHeader,
+  },
+  async fetch() {
+    const [topicsResult, partnersResult] = await Promise.allSettled([
+      this.$store.dispatch('fetchTopicsData'),
+      this.$store.dispatch('fetchPartnersData'),
+    ])
+
+    if (topicsResult.status === 'fulfilled') {
+      this.commitTopicsDataToStore(topicsResult)
+    }
+
+    if (partnersResult.status === 'fulfilled') {
+      this.commitPartnersDataToStore(partnersResult)
+    }
+  },
+  methods: {
+    commitTopicsDataToStore(result) {
+      const topicsData = camelizeKeys(result.value.data)
+
+      this.$store.commit('setTopicsData', topicsData.endpoints?.topics ?? {})
+    },
+    commitPartnersDataToStore(result) {
+      this.$store.commit('setPartnersData', camelizeKeys(result.value.data))
+    },
   },
 }
 </script>
---
+++ store/actions.js
@@ -0,0 +1,23 @@
+import { camelizeKeys } from 'humps'
+
+export default {
+  async nuxtServerInit({ commit, dispatch }) {
+    const sectionsResult = await dispatch('fetchSectionsData')
+
+    commit(
+      'setSectionsData',
+      camelizeKeys(sectionsResult.data).endpoints?.sections ?? {}
+    )
+  },
+  fetchSectionsData() {
+    return this.$axios.get('https://api.mirrormedia.mg/combo?endpoint=sections')
+  },
+  fetchTopicsData() {
+    return this.$axios.get('https://api.mirrormedia.mg/combo?endpoint=topics')
+  },
+  fetchPartnersData() {
+    return this.$axios.get(
+      'https://api.mirrormedia.mg/partners?max_results=25&page=1'
+    )
+  },
+}
