54be0497ced99ac99f5926e6f011599e21fe3193
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-05-06 18:34:35 +0800

    feat: UIArticleList component

---
+++ components/UIArticleList.vue
@@ -0,0 +1,79 @@
+<template>
+  <section class="list-wrapper">
+    <h1
+      v-if="listTitle !== ''"
+      class="title"
+      :style="{
+        color: listTitleColor,
+      }"
+      v-text="listTitle"
+    />
+    <ol v-if="listData.length > 0" class="list-wrapper__list list">
+      <li v-for="(item, i) in listData" :key="i" class="list__list-item">
+        <UIArticleCard
+          :href="item.href"
+          :img-src="item.imgSrc"
+          :img-text="item.imgText"
+          :img-text-background-color="item.imgTextBackgroundColor"
+          :info-title="item.infoTitle"
+          :info-description="item.infoDescription"
+        />
+      </li>
+    </ol>
+  </section>
+</template>
+
+<script>
+import UIArticleCard from './UIArticleCard.vue'
+export default {
+  components: {
+    UIArticleCard,
+  },
+  props: {
+    listTitle: {
+      type: String,
+      default: '',
+    },
+    listTitleColor: {
+      type: String,
+      default: 'black',
+    },
+    listData: {
+      type: Array,
+      default: () => [],
+    },
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+.title
+  padding: 0 32px
+  font-size: 24px
+  font-weight: 400
+  @include media-breakpoint-up(md)
+    padding: 0 16px
+  @include media-breakpoint-up(xl)
+    padding: 0
+
+.list-wrapper
+  &__list
+    margin: 20px 0 0 0
+
+.list
+  display: flex
+  flex-wrap: wrap
+  &__list-item
+    & + &
+      margin: 40px 0 0 0
+  @include media-breakpoint-up(md)
+    margin: -20px 0 0 -20px
+    &__list-item
+      max-width: calc(50% - 20px)
+      margin: 40px 0 0 20px
+      & + &
+        margin: 40px 0 0 20px
+  @include media-breakpoint-up(xl)
+    &__list-item
+      max-width: calc(33.33% - 20px)
+</style>
---
+++ components/__tests__/UIArticleList.spec.js
@@ -0,0 +1,64 @@
+import { shallowMount } from '@vue/test-utils'
+import UIArticleList from '../UIArticleList.vue'
+import UIArticleCard from '../UIArticleCard.vue'
+
+describe('list title', () => {
+  test('should render proper title from "listTitle" props', () => {
+    const listTitle = 'test title'
+    const wrapper = shallowMount(UIArticleList, {
+      propsData: {
+        listTitle,
+      },
+    })
+    const h1 = wrapper.find('h1')
+    expect(h1.text()).toBe(listTitle)
+  })
+  test('should render proper color from props "listTitleColor"', () => {
+    const listTitleColor = '#000000' // rgb(0, 0, 0)
+    const wrapper = shallowMount(UIArticleList, {
+      propsData: {
+        listTitle: 'test title',
+        listTitleColor,
+      },
+    })
+    const h1 = wrapper.find('h1')
+    expect(h1.element.style.color).toBe('rgb(0, 0, 0)')
+  })
+})
+
+describe('render list items', () => {
+  test('should render proper number of UIArticleCard that we provide from "listData" props', () => {
+    const listData = [{}, {}]
+    const wrapper = shallowMount(UIArticleList, {
+      propsData: {
+        listData,
+      },
+    })
+    const articles = wrapper.findAll(UIArticleCard)
+    expect(articles).toHaveLength(listData.length)
+  })
+  test('should deliver proper props to UIArticleCard that we provide from "listData" props', () => {
+    const listData = [
+      {
+        href: '/',
+        imgSrc: 'test imgSrc',
+        imgText: 'test imgText',
+        imgTextBackgroundColor: 'test imgTextBackgroundColor',
+        infoTitle: 'test infoTitle',
+        infoDescription: 'test infoDescription',
+      },
+    ]
+    const wrapper = shallowMount(UIArticleList, {
+      propsData: {
+        listData,
+      },
+    })
+    const article = wrapper.find(UIArticleCard)
+    const listItemMock = listData[0]
+    Object.entries(listItemMock).forEach((tuple) => {
+      const key = tuple[0]
+      const value = tuple[1]
+      expect(article.props()[key]).toBe(value)
+    })
+  })
+})
