e491a8631dd3262384ad8df76dbaf5610e544e0c
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-05-07 12:44:51 +0800

    feat: add layout/listing.vue

---
+++ layouts/listing.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="listing">
+    <nuxt />
+    <UIFooter class="footer" />
+  </div>
+</template>
+
+<script>
+import UIFooter from '~/components/UIFooter.vue'
+
+export default {
+  components: {
+    UIFooter,
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+.listing
+  background-color: #f2f2f2
+  padding: 0 0 60px 0
+  @include media-breakpoint-up(xl)
+    padding: 0
+
+.footer
+  margin: 0 32px
+  @include media-breakpoint-up(xl)
+    max-width: 1024px
+    margin: 60px auto 0 auto
+</style>
