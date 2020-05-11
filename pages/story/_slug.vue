2c8f1ef9310062dbdfb9e138a80724438d804c6f
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-05-11 12:38:41 +0800

    feat: add story/_slug.vue page in order to demo nuxt-link from listing

---
+++ pages/story/_slug.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    this is a story page, WIP
+    <br />
+    <nuxt-link to="/section/test">back to section page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
