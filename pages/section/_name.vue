40d58bd25fcfd5a9526a0647170fe2ee81e3c9dd
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-05-12 16:50:08 +0800

    feat: add name to page components

--- pages/section/_name.vue
+++ pages/section/_name.vue
@@ -14,6 +14,7 @@
 import UIArticleList from '~/components/UIArticleList.vue'
 
 export default {
+  name: 'Section',
   components: {
     UIArticleList,
   },
--- pages/story/_slug.vue
+++ pages/story/_slug.vue
@@ -7,7 +7,9 @@
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Story',
+}
 </script>
 
 <style lang="scss" scoped></style>
