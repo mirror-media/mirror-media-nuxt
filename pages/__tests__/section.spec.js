886da9d0f70ce42d1d55603ad7ca7bccc47b7c04
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-05-13 18:39:45 +0800

    test: replace shallowMount with createWrapper

--- pages/__tests__/section.spec.js
+++ pages/__tests__/section.spec.js
@@ -1,4 +1,3 @@
-import { shallowMount } from '@vue/test-utils'
 import page from '../section/_name.vue'
 import createWrapperHelper from '~/test/helpers/createWrapperHelper'
 
@@ -23,12 +22,12 @@ const createWrapper = createWrapperHelper({
 
 describe('stripHtmlTag method', () => {
   test('should strip html tags successfully', () => {
-    const wrapper = shallowMount(page)
+    const wrapper = createWrapper(page)
     const html = '<div><script></script><p>foo</p><p>bar</p><p>123</p></div>'
     expect(wrapper.vm.stripHtmlTag(html)).toBe('foobar123')
   })
   test('should return the same result if there is not html tags', () => {
-    const wrapper = shallowMount(page)
+    const wrapper = createWrapper(page)
     const html = 'foobar123'
     expect(wrapper.vm.stripHtmlTag(html)).toBe('foobar123')
   })
