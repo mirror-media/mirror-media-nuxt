bc63b723643f2cde5a24b922e48020ed5ab6a285
yeefun <szyeefun@gmail.com>
2020-05-13 10:53:55 +0800

    style: transform sass into scss

--- components/UIHeaderNavSection.vue
+++ components/UIHeaderNavSection.vue
@@ -82,101 +82,135 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
-$sections-color: ("news": #30bac8, "entertainment": #bf3284, "businessmoney": #009045, "people": #efa256, "videohub": #969696, "international": #911f27, "foodtravel": #eac151, "mafalda": #662d8e, "culture": #009245, "carandwatch": #003153, "external": #ee5a24)
+<style lang="scss" scoped>
+$sections-color: (
+  'news': #30bac8,
+  'entertainment': #bf3284,
+  'businessmoney': #009045,
+  'people': #efa256,
+  'videohub': #969696,
+  'international': #911f27,
+  'foodtravel': #eac151,
+  'mafalda': #662d8e,
+  'culture': #009245,
+  'carandwatch': #003153,
+  'external': #ee5a24,
+);
 
-.header-nav-section
-  font-size: 14px
+.header-nav-section {
+  font-size: 14px;
   // to hide scrollbar
-  height: 28px
-  overflow: hidden
+  height: 28px;
+  overflow: hidden;
 
-  display: flex
-  justify-content: center
-  @include media-breakpoint-up(xl)
-    background-color: #064f77
-    font-size: 16px
-    height: auto
-    overflow: visible
-  .container
+  display: flex;
+  justify-content: center;
+  @include media-breakpoint-up(xl) {
+    background-color: #064f77;
+    font-size: 16px;
+    height: auto;
+    overflow: visible;
+  }
+  .container {
     // to hide scrollbar
-    height: 48px
+    height: 48px;
 
-    display: flex
-    align-items: flex-start
-    justify-content: flex-start
-    overflow-x: auto
-    max-width: 1024px
-    margin-left: auto
-    margin-right: auto
-    text-align: center
-    @include media-breakpoint-up(xl)
-      width: 100%
-      height: 45px
-      justify-content: center
-      overflow: visible
-  .section
-    flex: 0 0 auto
-    padding: 7px 11px 5px 11px
-    position: relative
-    display: flex
-    align-items: center
-    justify-content: center
-    color: #808080
-    cursor: pointer
-    user-select: none
-    line-height: 1.15
-    @include media-breakpoint-up(xl)
-      color: #fff
-      padding: 0
-      line-height: 42px
-      min-width: calc(100% / 11)
-      border-top-width: 3px
-      border-top-style: solid
-      @each $name, $color in $sections-color
-        &--#{$name}
-          border-top-color: $color
-          &:hover
-            background-color: $color
-          & .section__dropdown a:hover
-            background-color: $color
-      &:hover .section__dropdown
-        display: block
-      &--home
-        display: none
-    &:hover
-      color: #34495e
-      @include media-breakpoint-up(xl)
-        color: #fff
-    &.active
-      color: #34495e
-      @include media-breakpoint-up(xl)
-        color: #fff
-      &::after
-        content: ''
-        position: absolute
-        left: 50%
-        transform: translateX(-50%)
-        bottom: 0
-        height: 3px
-        width: calc(100% - 22px)
-        background-color: #34495e
-        @include media-breakpoint-up(xl)
-          content: none
-    &--external
-      display: none
-      @include media-breakpoint-up(xl)
-        display: block
-    &__dropdown
-      position: absolute
-      display: none
-      width: 110px
-      left: 0
-      top: 100%
-      background-color: #333
-      text-align: center
-      a
-        line-height: 1.3
-        display: block
-        padding: 13px 10px
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    overflow-x: auto;
+    max-width: 1024px;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+    @include media-breakpoint-up(xl) {
+      width: 100%;
+      height: 45px;
+      justify-content: center;
+      overflow: visible;
+    }
+  }
+  .section {
+    flex: 0 0 auto;
+    padding: 7px 11px 5px 11px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #808080;
+    cursor: pointer;
+    user-select: none;
+    line-height: 1.15;
+    @include media-breakpoint-up(xl) {
+      color: #fff;
+      padding: 0;
+      line-height: 42px;
+      min-width: calc(100% / 11);
+      border-top-width: 3px;
+      border-top-style: solid;
+      @each $name, $color in $sections-color {
+        &--#{$name} {
+          border-top-color: $color;
+          &:hover {
+            background-color: $color;
+          }
+          & .section__dropdown a:hover {
+            background-color: $color;
+          }
+        }
+      }
+      &:hover .section__dropdown {
+        display: block;
+      }
+      &--home {
+        display: none;
+      }
+    }
+    &:hover {
+      color: #34495e;
+      @include media-breakpoint-up(xl) {
+        color: #fff;
+      }
+    }
+    &.active {
+      color: #34495e;
+      @include media-breakpoint-up(xl) {
+        color: #fff;
+      }
+      &::after {
+        content: '';
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        bottom: 0;
+        height: 3px;
+        width: calc(100% - 22px);
+        background-color: #34495e;
+        @include media-breakpoint-up(xl) {
+          content: none;
+        }
+      }
+    }
+    &--external {
+      display: none;
+      @include media-breakpoint-up(xl) {
+        display: block;
+      }
+    }
+    &__dropdown {
+      position: absolute;
+      display: none;
+      width: 110px;
+      left: 0;
+      top: 100%;
+      background-color: #333;
+      text-align: center;
+      a {
+        line-height: 1.3;
+        display: block;
+        padding: 13px 10px;
+      }
+    }
+  }
+}
 </style>
