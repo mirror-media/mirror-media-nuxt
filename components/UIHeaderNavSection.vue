365d8cb5805cdac83f1e02d64f6d9383e82fb188
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-05-14 12:42:05 +0800

    refactor: remove local sections color scss variable

--- components/UIHeaderNavSection.vue
+++ components/UIHeaderNavSection.vue
@@ -83,20 +83,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$sections-color: (
-  'news': #30bac8,
-  'entertainment': #bf3284,
-  'businessmoney': #009045,
-  'people': #efa256,
-  'videohub': #969696,
-  'international': #911f27,
-  'foodtravel': #eac151,
-  'mafalda': #662d8e,
-  'culture': #009245,
-  'carandwatch': #003153,
-  'external': #ee5a24,
-);
-
 .header-nav-section {
   font-size: 14px;
   // to hide scrollbar
