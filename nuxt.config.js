b2cde0a4b7a544c69fd48496c504c16e26471056
HY Tan <tempo0829@gmail.com>
2020-05-15 14:35:44 +0800

    feat: add request plugin (#53)
    * feat: add request plugin
    
    * refactor: use js native api
    
    * docs: add specific use case comment
    
    * refactor: use guard clauses in snakeCase function

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ module.exports = {
   plugins: [
     '~/plugins/vuePluginsGlobal.js',
     '~/plugins/vueDirectivesGlobal.js',
+    '~/plugins/requests/index.js',
   ],
   /*
    ** Nuxt.js Server Middleware
