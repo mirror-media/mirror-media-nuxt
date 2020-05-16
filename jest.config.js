fbe3f677766f777bfc1ac0cb9d2b76a697444e71
Kai-Wen, Hsiung <lion15945@gmail.com>
2020-05-16 20:52:09 +0800

    feat: section page color theme (#61)
    * feat: adapt theme colors in section page
    
    * test: add scss in jest-transform-stub to prevent errors

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '\\.(scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'jest-transform-stub',
   },
   collectCoverage: true,
