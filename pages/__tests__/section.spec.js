1f48051f57eb903baef0755b82e9be817d9705fd
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-05-17 17:24:44 +0800

    test: add component methods tests in section page

--- pages/__tests__/section.spec.js
+++ pages/__tests__/section.spec.js
@@ -68,3 +68,93 @@ describe('section data', () => {
     expect(wrapper.vm.currentSectionTitle).toBe(sectionTitleMock)
   })
 })
+
+describe('component methods', () => {
+  test('setListData', () => {
+    const sectionNameMock = 'test-name'
+    const sectionIdMock = 'test-id'
+    const sectionTitleMock = 'test-title'
+    const sectionStoreMock = {
+      data: {
+        items: [
+          {
+            name: sectionNameMock,
+            id: sectionIdMock,
+            title: sectionTitleMock,
+          },
+        ],
+      },
+    }
+
+    const idMock = 'id'
+    const slugMock = 'slug'
+    const imageUrlMock = 'imageurl'
+    const titleMock = 'title'
+    const briefMock = 'brief'
+    const briefHtmlMock = `<div>${briefMock}</div>`
+    const responseMock = {
+      items: [
+        {
+          name: sectionNameMock,
+          id: idMock,
+          slug: slugMock,
+          heroImage: {
+            image: {
+              resizedTargets: {
+                mobile: {
+                  url: imageUrlMock,
+                },
+              },
+            },
+          },
+          title: titleMock,
+          brief: {
+            html: briefHtmlMock,
+          },
+        },
+      ],
+    }
+
+    const wrapper = createWrapper(page, {
+      mocks: {
+        $route: {
+          params: {
+            name: sectionNameMock,
+          },
+        },
+        $store: {
+          state: {
+            sections: sectionStoreMock,
+          },
+        },
+      },
+    })
+
+    wrapper.vm.setListData(responseMock)
+    expect(wrapper.vm.listData).toEqual([
+      {
+        id: idMock,
+        href: `/story/${slugMock}`,
+        imgSrc: imageUrlMock,
+        imgText: sectionTitleMock,
+        imgTextBackgroundColor: undefined, // value from scss variable not available in jest
+        infoTitle: titleMock,
+        infoDescription: briefMock,
+      },
+    ])
+  })
+  test('setListDataTotal and listDataPageLimit computed by total', () => {
+    const totalMock = 1234
+    const responseMock = {
+      meta: {
+        total: totalMock,
+      },
+    }
+    const wrapper = createWrapper(page)
+    wrapper.vm.setListDataTotal(responseMock)
+    expect(wrapper.vm.listDataTotal).toBe(totalMock)
+    expect(wrapper.vm.listDataPageLimit).toBe(
+      Math.ceil(totalMock / wrapper.vm.listDataMaxResults)
+    )
+  })
+})
