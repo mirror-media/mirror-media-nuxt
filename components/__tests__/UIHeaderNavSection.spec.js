1c011f64b64baaa3cf58159d1faf0354eb2b55e0
yeefun <szyeefun@gmail.com>
2020-05-21 16:11:30 +0800

    refactor: replace shallowMount with createWrapperHelper

--- components/__tests__/UIHeaderNavSection.spec.js
+++ components/__tests__/UIHeaderNavSection.spec.js
@@ -1,76 +1,63 @@
-import { shallowMount } from '@vue/test-utils'
 import UIHeaderNavSection from '../UIHeaderNavSection.vue'
 
-const sectionDisplayed = {
+import createWrapperHelper from '~/test/helpers/createWrapperHelper'
+
+const mockSection = {
   name: 'culture',
   title: '文化',
   isFeatured: true,
   categories: [{ name: 'bookreview', title: '書評' }],
 }
-
-const partnerDisplayed = {
+const mockPartner = {
   name: 'healthnews',
   display: '健康醫療網新聞',
   public: true,
 }
+const createWrapper = createWrapperHelper({
+  propsData: {
+    sections: [mockSection],
+    partners: [mockPartner],
+  },
+  stubs: ['nuxt-link'],
+})
 
 describe('normal section nav', () => {
-  const wrapper = shallowMount(UIHeaderNavSection, {
-    propsData: {
-      sections: [sectionDisplayed],
-      partners: [],
-    },
-    stubs: ['nuxt-link'],
-  })
-
   test('render the proper section link', () => {
-    const link = wrapper.find(`[to="/section/${sectionDisplayed.name}"]`)
-    expect(link.text()).toBe(sectionDisplayed.title)
+    const wrapper = createWrapper(UIHeaderNavSection)
+
+    const link = wrapper.find(`[to="/section/${mockSection.name}"]`)
+    expect(link.text()).toBe(mockSection.title)
   })
 
   test('render the proper category link', () => {
-    const [category] = sectionDisplayed.categories
+    const wrapper = createWrapper(UIHeaderNavSection)
+
+    const [category] = mockSection.categories
     const link = wrapper.find(`[to="/category/${category.name}"]`)
     expect(link.text()).toBe(category.title)
   })
 })
 
 describe('external section nav', () => {
-  const wrapper = shallowMount(UIHeaderNavSection, {
-    propsData: {
-      sections: [],
-      partners: [partnerDisplayed],
-    },
-    stubs: ['nuxt-link'],
-  })
-
   test('render the proper partner link', () => {
-    const link = wrapper.find(`[to="/externals/${partnerDisplayed.name}"]`)
-    expect(link.text()).toBe(partnerDisplayed.display)
+    const wrapper = createWrapper(UIHeaderNavSection)
+
+    const link = wrapper.find(`[to="/externals/${mockPartner.name}"]`)
+    expect(link.text()).toBe(mockPartner.display)
   })
 })
 
 describe('emitGA method', () => {
-  const wrapper = shallowMount(UIHeaderNavSection, {
-    propsData: {
-      sections: [sectionDisplayed],
-      partners: [partnerDisplayed],
-    },
-    stubs: ['nuxt-link'],
-  })
-
-  beforeEach(function resetEmitted() {
-    wrapper._emitted.sendGA = []
-  })
-
   test('with a proper argument when users click a section link', () => {
-    const linkNormal = wrapper.find(`[to="/section/${sectionDisplayed.name}"]`)
+    const wrapper = createWrapper(UIHeaderNavSection)
+
+    const linkNormal = wrapper.find(`[to="/section/${mockSection.name}"]`)
     linkNormal.trigger('click')
     expect(wrapper.emitted().sendGA[0]).toEqual([
       {
         eventCategory: 'header',
         eventAction: 'click',
-        eventLabel: `section ${sectionDisplayed.name}`,
+        eventLabel: `section ${mockSection.name}`,
       },
     ])
 
@@ -86,7 +73,9 @@ describe('emitGA method', () => {
   })
 
   test('with a proper argument when users click a category link', () => {
-    const [category] = sectionDisplayed.categories
+    const wrapper = createWrapper(UIHeaderNavSection)
+
+    const [category] = mockSection.categories
     const link = wrapper.find(`[to="/category/${category.name}"]`)
     link.trigger('click')
     expect(wrapper.emitted().sendGA[0]).toEqual([
@@ -99,13 +88,15 @@ describe('emitGA method', () => {
   })
 
   test('with a proper argument when users click a partner link', () => {
-    const link = wrapper.find(`[to="/externals/${partnerDisplayed.name}"]`)
+    const wrapper = createWrapper(UIHeaderNavSection)
+
+    const link = wrapper.find(`[to="/externals/${mockPartner.name}"]`)
     link.trigger('click')
     expect(wrapper.emitted().sendGA[0]).toEqual([
       {
         eventCategory: 'header',
         eventAction: 'click',
-        eventLabel: `external ${partnerDisplayed.name}`,
+        eventLabel: `external ${mockPartner.name}`,
       },
     ])
   })
@@ -113,13 +104,7 @@ describe('emitGA method', () => {
 
 describe('markup', () => {
   test('render correctly', () => {
-    const wrapper = shallowMount(UIHeaderNavSection, {
-      propsData: {
-        sections: [sectionDisplayed],
-        partners: [partnerDisplayed],
-      },
-      stubs: ['nuxt-link'],
-    })
+    const wrapper = createWrapper(UIHeaderNavSection)
 
     expect(wrapper.element).toMatchSnapshot()
   })
