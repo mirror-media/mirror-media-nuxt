475eb63136f335eedc04923ade8e6304d096666c
yeefun <szyeefun@gmail.com>
2020-05-28 23:29:44 +0800

    refactor: let UIHeaderNavTopic use sub-brand constant

--- components/__tests__/UIHeaderNavTopic.spec.js
+++ components/__tests__/UIHeaderNavTopic.spec.js
@@ -2,29 +2,59 @@ import UIHeaderNavTopic from '../UIHeaderNavTopic.vue'
 
 import createWrapperHelper from '~/test/helpers/createWrapperHelper'
 
-const mockTopic = {
-  name: '人造地獄',
-  id: '5d8c26c23c4cad5f87630d37',
-}
 const createWrapper = createWrapperHelper({
   propsData: {
-    topics: [mockTopic],
+    topics: [],
+    subBrands: [],
   },
   stubs: ['nuxt-link'],
 })
 
+const mockTopic = {
+  name: '人造地獄',
+  id: '5d8c26c23c4cad5f87630d37',
+}
+const mockSubBrand = {
+  name: 'mirrorvoice',
+  title: '鏡好聽',
+  href: 'https://voice.mirrorfiction.com/',
+}
+
 describe('topic nav', () => {
   test('render the proper topic link', () => {
-    const wrapper = createWrapper(UIHeaderNavTopic)
+    const wrapper = createWrapper(UIHeaderNavTopic, {
+      propsData: {
+        topics: [mockTopic],
+        subBrands: [],
+      },
+    })
 
     const linkNormal = wrapper.find(`[to="/topic/${mockTopic.id}"]`)
     expect(linkNormal.text()).toBe(mockTopic.name)
   })
 })
 
+describe('sub-brand nav', () => {
+  test('render the proper sub-brand link', () => {
+    const wrapper = createWrapper(UIHeaderNavTopic, {
+      propsData: {
+        subBrands: [mockSubBrand],
+      },
+    })
+
+    const subBrandLink = wrapper.get(`[href="${mockSubBrand.href}"]`)
+    expect(subBrandLink.get('img').attributes().alt).toBe(mockSubBrand.title)
+  })
+})
+
 describe('emitGA method', () => {
   test('with a proper argument when users click a topic link', () => {
-    const wrapper = createWrapper(UIHeaderNavTopic)
+    const wrapper = createWrapper(UIHeaderNavTopic, {
+      propsData: {
+        topics: [mockTopic],
+        subBrands: [],
+      },
+    })
 
     const linkNormal = wrapper.find(`[to="/topic/${mockTopic.id}"]`)
     linkNormal.trigger('click')
@@ -48,44 +78,20 @@ describe('emitGA method', () => {
   })
 
   test('with a proper argument when users click a sub-brand link', () => {
-    const wrapper = createWrapper(UIHeaderNavTopic)
+    const wrapper = createWrapper(UIHeaderNavTopic, {
+      propsData: {
+        subBrands: [mockSubBrand],
+      },
+    })
 
-    const voiceLink = wrapper.find('[href="https://voice.mirrorfiction.com/"]')
-    voiceLink.trigger('click')
+    const subBrandLink = wrapper.get(`[href="${mockSubBrand.href}"]`)
+    subBrandLink.trigger('click')
     expect(wrapper.emitted().sendGA[0]).toEqual([
       {
         eventCategory: 'header',
         eventAction: 'click',
-        eventLabel: 'section mirrorvoice',
+        eventLabel: `section ${mockSubBrand.name}`,
       },
     ])
-
-    const fictionLink = wrapper.find('[href="https://www.mirrorfiction.com/"]')
-    fictionLink.trigger('click')
-    expect(wrapper.emitted().sendGA[1]).toEqual([
-      {
-        eventCategory: 'header',
-        eventAction: 'click',
-        eventLabel: 'section mirrorfiction',
-      },
-    ])
-
-    const readrLink = wrapper.find('[href="https://www.readr.tw/"]')
-    readrLink.trigger('click')
-    expect(wrapper.emitted().sendGA[2]).toEqual([
-      {
-        eventCategory: 'header',
-        eventAction: 'click',
-        eventLabel: 'section readr',
-      },
-    ])
-  })
-})
-
-describe('markup', () => {
-  test('render correctly', () => {
-    const wrapper = createWrapper(UIHeaderNavTopic)
-
-    expect(wrapper.element).toMatchSnapshot()
   })
 })
