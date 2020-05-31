2170f45bdd9b3b15c151f03009ec00b35a815552
HY Tan <hsuehyungtan@gmail.com>
2020-05-31 15:41:38 +0800

    refactor: combine ContainerYoutubeSubscribe with UIYoutubeSubscribe

--- components/UIYoutubeSubscribe.vue
+++ components/UIYoutubeSubscribe.vue
@@ -7,6 +7,8 @@
   />
 </template>
 <script>
+// Document: https://developers.google.com/youtube/youtube_subscribe_button
+
 export default {
   name: 'UIYoutubeSubscribe',
   props: {
@@ -15,6 +17,26 @@ export default {
       required: true,
     },
   },
+  mounted() {
+    window.addEventListener('load', this.init)
+  },
+  beforeDestroy() {
+    window.removeEventListener('load', this.init)
+  },
+  methods: {
+    init() {
+      const hasInjectedSDK =
+        window.gapi || document.querySelector('#js-yt-platform')
+      if (!hasInjectedSDK) {
+        const youtubePlatformScript = document.createElement('script')
+        youtubePlatformScript.setAttribute('id', 'js-yt-platform')
+        youtubePlatformScript.setAttribute(
+          'src',
+          'https://apis.google.com/js/platform.js'
+        )
+        document.head.appendChild(youtubePlatformScript)
+      }
+    },
+  },
 }
 </script>
-<style lang="scss" scoped></style>
