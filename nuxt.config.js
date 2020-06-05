0de3499011544a93eaac88db5e56605627cf1f49
HY Tan <tempo0829@gmail.com>
2020-06-05 19:22:45 +0800

    feat: add favicon setting (#109)

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,8 +13,30 @@ module.exports = {
         name: 'description',
         content: process.env.npm_package_description || '',
       },
+      { name: 'theme-color', content: '#ffffff' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+    ],
   },
   /*
    ** Customize the progress-bar color
