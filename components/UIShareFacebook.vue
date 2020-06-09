49fc28c734cd339898fbefb940137ab977e536c3
HY Tan <tempo0829@gmail.com>
2020-06-09 12:34:02 +0800

    feat: add components for facebook and line share (#105)
    * feat: add components for facebook and line share
    
    * style: SFC block space
    
    * refactor: simplify shareUrl logic
    
    * test: use shared createWrapperHelper
    
    * test: use spyOn to mock window.location

---
+++ components/UIShareFacebook.vue
@@ -0,0 +1,51 @@
+<template>
+  <a
+    :href="`https://www.facebook.com/share.php?u=${shareUrl}`"
+    target="_blank"
+    rel="noopener noreferrer"
+    @click="$emit('click')"
+  >
+    <picture>
+      <source
+        srcset="~/assets/facebook_logo_official_72.webp"
+        type="image/webp"
+      />
+      <img
+        src="~/assets/facebook_logo_official_72.png"
+        alt="分享至 Facebook"
+        loading="lazy"
+      />
+    </picture>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'UIShareFacebook',
+  props: {
+    url: {
+      type: String,
+      default: undefined,
+    },
+  },
+  data() {
+    return {
+      isMounted: false,
+    }
+  },
+  computed: {
+    shareUrl() {
+      return this.url || (this.isMounted && location.href)
+    },
+  },
+  mounted() {
+    this.isMounted = true
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+picture {
+  display: block;
+}
+</style>
