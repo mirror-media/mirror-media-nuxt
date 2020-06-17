84323ab9f88aa4e364b69aad2d6050e4eac6fa9f
yeefun <szyeefun@gmail.com>
2020-06-17 10:38:20 +0800

    refactor: rename

--- components/ContainerHeader.vue
+++ components/ContainerHeader.vue
@@ -192,31 +192,35 @@ export default {
       this.$ga.event(param)
     },
     activeTheNavSection(path) {
-      const prefixOfStory = this.regexOfPrefix('/story/')
+      const regexWithPrefixOfStory = this.generateRegexWithPrefixOf('/story/')
       // 文章頁也需要設定 sectionName，但不是在這邊做，而需移到 story page，故 return
-      if (this.hasPrefix(path, prefixOfStory)) {
+      if (this.hasPrefix(path, regexWithPrefixOfStory)) {
         return
       }
 
-      const prefixOfSection = this.regexOfPrefix('/section/')
-      const prefixOfCategory = this.regexOfPrefix('/category/')
+      const regexWithPrefixOfSection = this.generateRegexWithPrefixOf(
+        '/section/'
+      )
+      const regexWithPrefixOfCategory = this.generateRegexWithPrefixOf(
+        '/category/'
+      )
       let sectionName
 
       if (path === '/') {
         sectionName = 'home'
-      } else if (this.hasPrefix(path, prefixOfSection)) {
-        sectionName = this.removePrefix(path, prefixOfSection)
+      } else if (this.hasPrefix(path, regexWithPrefixOfSection)) {
+        sectionName = this.removePrefix(path, regexWithPrefixOfSection)
         if (sectionName === 'topic') {
           return
         }
-      } else if (this.hasPrefix(path, prefixOfCategory)) {
-        const categoryName = this.removePrefix(path, prefixOfCategory)
+      } else if (this.hasPrefix(path, regexWithPrefixOfCategory)) {
+        const categoryName = this.removePrefix(path, regexWithPrefixOfCategory)
         sectionName = this.sectionByCategoryName(categoryName).name
       }
 
       this.setSectionName(sectionName)
     },
-    regexOfPrefix(string) {
+    generateRegexWithPrefixOf(string) {
       return new RegExp(`^${string}`, 'i')
     },
     hasPrefix(string, prefix) {
