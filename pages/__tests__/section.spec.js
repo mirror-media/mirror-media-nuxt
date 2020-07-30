495de649b642214f1c9c26d62dd5aee69fd0c078
kwhsiung <kaiwenhsiung@gmail.com>
2020-07-30 18:03:31 +0800

    test(meta): add tests of meta tags in secton page

--- pages/__tests__/section.spec.js
+++ pages/__tests__/section.spec.js
@@ -1,6 +1,7 @@
 import page from '../section/_name.vue'
 import UIArticleList from '~/components/UIArticleList.vue'
 import createWrapperHelper from '~/test/helpers/createWrapperHelper'
+import { SITE_TITLE } from '~/constants'
 
 const createWrapper = createWrapperHelper({
   mocks: {
@@ -162,5 +163,116 @@ describe('component methods', () => {
     expect(wrapper.vm.listDataPageLimit).toBe(
       Math.ceil(totalMock / wrapper.vm.listDataMaxResults)
     )
+  })
+})
+
+describe('meta', function () {
+  test('head() method in vm option should work correctly', function () {
+    const sectionNameMock = 'test-name'
+    const sectionIdMock = 'test-id'
+    const sectionTitleMock = 'test-title'
+    const sectionDescriptionMock = 'test-description'
+    const sectionStoreMock = {
+      data: {
+        items: [
+          {
+            name: sectionNameMock,
+            id: sectionIdMock,
+            title: sectionTitleMock,
+            description: sectionDescriptionMock,
+          },
+        ],
+      },
+    }
+    const wrapper = createWrapper(page, {
+      mocks: {
+        $route: {
+          params: {
+            name: sectionNameMock,
+          },
+        },
+        $store: {
+          state: {
+            sections: sectionStoreMock,
+          },
+        },
+      },
+    })
+    const head = wrapper.vm.$options.head.bind(wrapper.vm)
+    const metaResults = head()
+
+    // titles
+    const titleMetas = ['og:title', 'twitter:title']
+    const titleExpected = `${sectionTitleMock} - ${SITE_TITLE}`
+    expect(metaResults.title).toBe(titleExpected)
+    titleMetas.forEach(function assertion(metaName) {
+      const metaObject = metaResults.meta.find((d) => d.hid === metaName)
+      expect(metaObject.content).toBe(titleExpected)
+    })
+
+    // descriptions
+    const descriptionMetas = [
+      'description',
+      'og:description',
+      'twitter:description',
+    ]
+    descriptionMetas.forEach(function assertion(metaName) {
+      const metaObject = metaResults.meta.find((d) => d.hid === metaName)
+      expect(metaObject.content).toBe(sectionDescriptionMock)
+    })
+
+    // og:url
+    expect(metaResults.meta.find((d) => d.hid === 'og:url').content).toBe(
+      `https://www.mirrormedia.mg/section/${sectionNameMock}`
+    )
+
+    // section-name
+    expect(metaResults.meta.find((d) => d.hid === 'section-name').content).toBe(
+      sectionNameMock
+    )
+  })
+
+  test('description should be the default value if description in section store is empty', function () {
+    const sectionStoreMock = {
+      data: {
+        items: [
+          {
+            name: 'test-name',
+            id: 'test-id',
+            title: 'test-title',
+            description: '',
+          },
+        ],
+      },
+    }
+    const wrapper = createWrapper(page, {
+      mocks: {
+        $route: {
+          params: {
+            name: 'test-name',
+          },
+        },
+        $store: {
+          state: {
+            sections: sectionStoreMock,
+          },
+        },
+      },
+    })
+    const head = wrapper.vm.$options.head.bind(wrapper.vm)
+    const metaResults = head()
+
+    // descriptions
+    const descriptionMetas = [
+      'description',
+      'og:description',
+      'twitter:description',
+    ]
+    descriptionMetas.forEach(function assertion(metaName) {
+      const metaObject = metaResults.meta.find((d) => d.hid === metaName)
+      expect(metaObject.content).toBe(
+        '鏡傳媒以台灣為基地，是一跨平台綜合媒體，包含《鏡週刊》以及下設五大分眾內容的《鏡傳媒》網站，刊載時事、財經、人物、國際、文化、娛樂、美食旅遊、精品鐘錶等深入報導及影音內容。我們以「鏡」為名，務求反映事實、時代與人性。'
+      )
+    })
   })
 })
