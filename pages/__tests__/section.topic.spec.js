89cfe76dd65a7e0e399caae91469ea2ed5e3effe
Kai-Wen, Hsiung <lion15945@gmail.com>
2020-07-31 16:34:11 +0800

    feat(meta): add meta variation settings between default meta in section/topic page (#180)

--- pages/__tests__/section.topic.spec.js
+++ pages/__tests__/section.topic.spec.js
@@ -1,5 +1,6 @@
 import page from '../section/topic.vue'
 import createWrapperHelper from '~/test/helpers/createWrapperHelper'
+import { SITE_TITLE } from '@/constants'
 
 const createWrapper = createWrapperHelper({
   mocks: {
@@ -124,6 +125,34 @@ describe('component methods', () => {
     expect(wrapper.vm.listDataTotal).toBe(totalMock)
     expect(wrapper.vm.listDataPageLimit).toBe(
       Math.ceil(totalMock / wrapper.vm.listDataMaxResults)
+    )
+  })
+})
+
+describe('meta', function () {
+  test('head() method in vm option should work correctly', function () {
+    const wrapper = createWrapper(page)
+
+    const head = wrapper.vm.$options.head.bind(wrapper.vm)
+    const metaResults = head()
+
+    // titles
+    const titleMetas = ['og:title', 'twitter:title']
+    const titleExpected = `Topic - ${SITE_TITLE}`
+    expect(metaResults.title).toBe(titleExpected)
+    titleMetas.forEach(function assertion(metaName) {
+      const metaObject = metaResults.meta.find((d) => d.hid === metaName)
+      expect(metaObject.content).toBe(titleExpected)
+    })
+
+    // og:url
+    expect(metaResults.meta.find((d) => d.hid === 'og:url').content).toBe(
+      'https://www.mirrormedia.mg/section/topic'
+    )
+
+    // section-name
+    expect(metaResults.meta.find((d) => d.hid === 'section-name').content).toBe(
+      'other'
     )
   })
 })
