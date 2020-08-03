fdd8e0a7583437bfa696cc6de1038a722e0dfe4b
kwhsiung <kaiwenhsiung@gmail.com>
2020-08-03 11:30:31 +0800

    feat(meta): add meta variation settings between default meta in author page

--- pages/__tests__/author.spec.js
+++ pages/__tests__/author.spec.js
@@ -1,5 +1,6 @@
 import page from '../author/_id.vue'
 import createWrapperHelper from '~/test/helpers/createWrapperHelper'
+import { SITE_TITLE } from '~/constants'
 
 const createWrapper = createWrapperHelper({
   mocks: {
@@ -116,5 +117,44 @@ describe('component methods', () => {
     const wrapper = createWrapper(page)
     wrapper.vm.setAuthorName(responseMock)
     expect(wrapper.vm.authorName).toBe(authorNameMock)
+  })
+})
+
+describe('meta', function () {
+  test('head() method in vm option should work correctly', function () {
+    const authorNameMock = 'author-name-mock'
+    const authorIdMock = 'author-id-mock'
+
+    const wrapper = createWrapper(page, {
+      data() {
+        return {
+          authorName: authorNameMock,
+        }
+      },
+      mocks: {
+        $route: {
+          params: {
+            id: authorIdMock,
+          },
+        },
+      },
+    })
+
+    const head = wrapper.vm.$options.head.bind(wrapper.vm)
+    const metaResults = head()
+
+    // titles
+    const titleMetas = ['og:title', 'twitter:title']
+    const titleExpected = `${authorNameMock} - ${SITE_TITLE}`
+    expect(metaResults.title).toBe(titleExpected)
+    titleMetas.forEach(function assertion(metaName) {
+      const metaObject = metaResults.meta.find((d) => d.hid === metaName)
+      expect(metaObject.content).toBe(titleExpected)
+    })
+
+    // og:url
+    expect(metaResults.meta.find((d) => d.hid === 'og:url').content).toBe(
+      `https://www.mirrormedia.mg/author/${authorIdMock}`
+    )
   })
 })
