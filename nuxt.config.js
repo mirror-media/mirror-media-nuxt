5adf5079bd7cbc27f07763793e22783812b404da
kwhsiung <kaiwenhsiung@gmail.com>
2020-08-25 18:45:05 +0800

    feat(page-level-cache): add page-level-cache just by redis

--- nuxt.config.js
+++ nuxt.config.js
@@ -192,7 +192,42 @@ module.exports = {
     '@nuxtjs/axios',
     '@nuxtjs/dayjs',
     'nuxt-user-agent',
+    '@mirror-media/nuxt-ssr-cache',
   ],
+  // config for @mirror-media/nuxt-ssr-cache
+  cache: {
+    /*
+     ** Workaround, pages array must exist and not empty,
+     ** see: https://github.com/arash16/nuxt-ssr-cache/blob/master/src/middleware.js#L34
+     */
+    pages: ['/'],
+
+    key(route) {
+      // We should configure cache pages path right here.
+      const cachePages = ['/']
+
+      const shouldCacheCurrentRoute = cachePages.some((pat) =>
+        pat instanceof RegExp ? pat.test(route) : route.startsWith(pat)
+      )
+      if (shouldCacheCurrentRoute) {
+        const prefixForGrep = 'mirror-media-nuxt:'
+        return `${prefixForGrep}${route}`
+      }
+    },
+    store: {
+      type: 'redis',
+      /*
+       ** readHost and writeHost working in @mirror-media/nuxt-ssr-cache only,
+       ** for redis write/read master-slave replication
+       ** Not working in the original nuxt-ssr-cache package
+       */
+      readHost: require('./configs/config').REDIS_READ_HOST,
+      writeHost: require('./configs/config').REDIS_WRITE_HOST,
+      port: require('./configs/config').REDIS_PORT,
+      auth_pass: require('./configs/config').REDIS_AUTH,
+      ttl: 60 * 5,
+    },
+  },
   /*
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
