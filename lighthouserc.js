c74c644fe5988d66eb3d6a1b953634337f957d27
kwhsiung <kaiwenhsiung@gmail.com>
2020-09-01 11:51:03 +0800

    ci: add lighthouserc.js

---
+++ lighthouserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'npm run start',
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
+        'categories:pwa': ['error', { minScore: 0.9 }],
+      },
+    },
+  },
+}
