eecc54a94c608c4faad01ffa6e30e0dddc576950
kwhsiung <kaiwenhsiung@gmail.com>
2020-09-02 17:38:46 +0800

    ci: adjust lhci performance min score to 60

--- lighthouserc.js
+++ lighthouserc.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'categories:performance': ['error', { minScore: 0.8 }],
+        'categories:performance': ['error', { minScore: 0.6 }],
         'categories:accessibility': ['error', { minScore: 0.9 }],
         'categories:best-practices': ['error', { minScore: 0.9 }],
         'categories:seo': ['error', { minScore: 0.95 }],
