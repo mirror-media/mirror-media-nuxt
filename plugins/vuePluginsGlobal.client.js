7a6ed6985b26b21a3435431b6384fda64bbd7370
kwhsiung <kaiwenhsiung@gmail.com>
2020-09-08 17:18:49 +0800

    refactor: provide disable ad for GPT and MicroAd

--- plugins/vuePluginsGlobal.client.js
+++ plugins/vuePluginsGlobal.client.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import GPTAD from './gpt-ad'
-import { GPT_MODE } from '~/configs/config'
+import { GPT_MODE, IS_AD_DISABLE } from '~/configs/config'
 
 Vue.use(GPTAD, {
   adNetwork: '40175602',
   mode: GPT_MODE,
+  isAdDisable: IS_AD_DISABLE,
 })
