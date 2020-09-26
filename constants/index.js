3df94ca864e88628dd987aabb8349a7473007a5a
yeefun <szyeefun@gmail.com>
2020-09-26 18:54:57 +0800

    refactor: use a single export

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const READR_URL = 'https://www.readr.tw'
+const READR_URL = 'https://www.readr.tw'
 
 const LINE_LINK = {
   name: 'line',
@@ -24,7 +24,7 @@ const EMAIL_LINK = {
   name: 'email',
   href: 'mailto:mirror885@mirrormedia.mg',
 }
-export const SOCIAL_MEDIA_LINKS = [
+const SOCIAL_MEDIA_LINKS = [
   LINE_LINK,
   WEIBO_LINK,
   FACEBOOK_LINK,
@@ -33,7 +33,7 @@ export const SOCIAL_MEDIA_LINKS = [
   EMAIL_LINK,
 ]
 
-export const SUBSCRIBE_LINK = {
+const SUBSCRIBE_LINK = {
   name: 'subscribe',
   title: '訂閱鏡週刊',
   href:
@@ -44,7 +44,7 @@ const MAGAZINE_LINK = {
   title: '訂閱電子雜誌',
   href: 'https://mybook.taiwanmobile.com/contentGroup/MIR0100100001',
 }
-export const AUTH_LINK = {
+const AUTH_LINK = {
   name: 'auth',
   title: '內容授權',
   href: 'https://www.mirrormedia.mg/story/webauthorize/',
@@ -64,7 +64,7 @@ const DOWNLOAD_APP_LINK = {
   title: '下載APP',
   href: 'https://www.mirrormedia.mg/story/20161228corpmkt001/',
 }
-export const OTHER_LINKS = [
+const OTHER_LINKS = [
   SUBSCRIBE_LINK,
   MAGAZINE_LINK,
   AUTH_LINK,
@@ -88,13 +88,9 @@ const READR_LINK = {
   title: 'READr 讀+',
   href: READR_URL,
 }
-export const SUB_BRAND_LINKS = [
-  MIRRORVOICE_LINK,
-  MIRRORFICTION_LINK,
-  READR_LINK,
-]
+const SUB_BRAND_LINKS = [MIRRORVOICE_LINK, MIRRORFICTION_LINK, READR_LINK]
 
-export const VIDEOHUB_CATEGORIES_PLAYLIST_MAPPING = {
+const VIDEOHUB_CATEGORIES_PLAYLIST_MAPPING = {
   // 鏡封面
   video_coverstory: 'PLftq_bkhPR3ZtDGBhyqVGObQXazG_O3M3',
   // 鏡娛樂
@@ -115,7 +111,20 @@ export const VIDEOHUB_CATEGORIES_PLAYLIST_MAPPING = {
   video_carandwatch: 'PLgvIJQ8OtT8LOdwVF4P9hdQiuf6uAiwb6',
 }
 
-export const SITE_TITLE = '鏡週刊 Mirror Media'
-export const SITE_URL = 'https://www.mirrormedia.mg'
-export const SITE_OG_IMAGE =
+const SITE_TITLE = '鏡週刊 Mirror Media'
+const SITE_URL = 'https://www.mirrormedia.mg'
+const SITE_OG_IMAGE =
   'https://www.mirrormedia.mg/assets/mirrormedia/notImage.png'
+
+export {
+  READR_URL,
+  SOCIAL_MEDIA_LINKS,
+  SUBSCRIBE_LINK,
+  AUTH_LINK,
+  OTHER_LINKS,
+  SUB_BRAND_LINKS,
+  VIDEOHUB_CATEGORIES_PLAYLIST_MAPPING,
+  SITE_TITLE,
+  SITE_URL,
+  SITE_OG_IMAGE,
+}
