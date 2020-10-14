c558ebecaad292804e8cba63494643764619b8c7
yeefun <szyeefun@gmail.com>
2020-10-14 14:19:53 +0800

    test: remove unneeded config

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,6 @@ module.exports = {
     '\\.(scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'jest-transform-stub',
   },
-  collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue',
