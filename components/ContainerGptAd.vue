800d07aefcc4f3f5d8903feb156301fc62f131b3
yeefun <szyeefun@gmail.com>
2020-10-29 16:16:55 +0800

    fix: fix ADs layout

--- components/ContainerGptAd.vue
+++ components/ContainerGptAd.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container-gpt-ad">
     <GPTAD
       v-if="shouldOpenAd"
       :adUnit="adData.adUnit"
@@ -85,3 +85,16 @@ function validateAdData(data) {
 
 export { validateAdData }
 </script>
+
+<style lang="scss" scoped>
+/**
+ * 廣告有時會替換掉原本 <GPTAD> 元件裡頭的根元素 <div>
+ * 因此用 ::v-deep，並不限定所指定的元素類型（*）
+ * 以確保能選擇到 .container-gpt-ad 的直接子元素
+ */
+.container-gpt-ad::v-deep > * {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
