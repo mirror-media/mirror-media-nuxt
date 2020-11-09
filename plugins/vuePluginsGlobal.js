dd2dfb8b3b7affa44f77bf6dfcf9f27bf416a2a3
kwhsiung <kaiwenhsiung@gmail.com>
2020-11-09 17:27:09 +0800

    build: add vuelidate package

--- plugins/vuePluginsGlobal.js
+++ plugins/vuePluginsGlobal.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import VueLazyload from 'vue-lazyload'
+import Vuelidate from 'vuelidate'
 import loadingImg from '~/assets/loading.gif'
 
 Vue.use(VueLazyload, {
   loading: loadingImg,
   lazyComponent: true,
 })
+Vue.use(Vuelidate)
