b21a577c3932b8c1c24ead95d2c3c57079f3e78b
kwhsiung <kaiwenhsiung@gmail.com>
2020-11-09 17:31:32 +0800

    feat: import vuelidate just for component rather than globally

---
+++ components/UIMembershipEmailInput.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { validationMixin } from 'vuelidate'
+export default {
+  mixins: [validationMixin],
+}
+</script>
+
+<style scoped></style>
