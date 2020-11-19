32bafbf18fb5c5a780212d36dd5112c622abbd11
yeefun <szyeefun@gmail.com>
2020-11-19 11:32:26 +0800

    refactor(culture-post): use the better way to render article body

---
+++ components/culture-post/ContentHandler.vue
@@ -0,0 +1,195 @@
+<script>
+import UiAnnotation from './UiAnnotation.vue'
+import ContainerParagraphWithAnnotation from '~/components/ContainerParagraphWithAnnotation.vue'
+
+import SvgQuoteIcon from '~/assets/quote.svg?inline'
+
+export default {
+  name: 'ContentHandler',
+
+  functional: true,
+
+  props: {
+    item: {
+      type: Object,
+      required: true,
+      default: () => ({}),
+    },
+  },
+
+  render(_, { props }) {
+    const { type, content: contents = [], id } = props.item
+    const content = contents[0]
+
+    if (!content) {
+      return undefined
+    }
+
+    switch (type) {
+      case 'header-one':
+        return <h2 id={`header-${id}`} domPropsInnerHTML={content} />
+
+      case 'header-two':
+        return <h3 domPropsInnerHTML={content} />
+
+      case 'image': {
+        const { alignment, tablet = {}, description } = content
+
+        return (
+          // eslint-disable-next-line lines-around-comment
+          // 不需要 'center' 來判斷樣式，故排除
+          <figure class={alignment !== 'center' && alignment}>
+            <img v-lazy={tablet.url} alt="" />
+
+            {description && <figcaption>{description}</figcaption>}
+          </figure>
+        )
+      }
+
+      case 'blockquote':
+        return (
+          <div class="quote">
+            <div class="quote-icon-wrapper">
+              <SvgQuoteIcon />
+            </div>
+
+            <blockquote domPropsInnerHTML={content}></blockquote>
+          </div>
+        )
+
+      case 'annotation':
+        return (
+          <ContainerParagraphWithAnnotation
+            class="g-story-paragraph"
+            content={content}
+            scopedSlots={{
+              default: ({ data }) => (
+                <UiAnnotation key={data.id} content={data} />
+              ),
+            }}
+          />
+        )
+
+      case 'unstyled':
+        return <p domPropsInnerHTML={content} />
+
+      default:
+        return undefined
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+$quote-color: #4a90e2;
+
+.article-body {
+  h2,
+  h3 {
+    font-family: source-han-serif-tc, 'Songti TC', serif;
+    font-weight: 700;
+    line-height: 1.5;
+    margin-top: 60px;
+  }
+
+  h2 {
+    font-size: 28px;
+    @include media-breakpoint-up(lg) {
+      font-size: 30px;
+    }
+  }
+
+  h3 {
+    font-size: 18px;
+  }
+
+  figure {
+    width: 100%;
+    padding-left: 0;
+    padding-right: 0;
+    @include media-breakpoint-up(md) {
+      &.left,
+      &.right {
+        clear: both;
+        width: 300px;
+        margin-bottom: 35px;
+
+        figcaption {
+          margin-left: 0;
+          margin-right: 0;
+        }
+      }
+
+      &.left {
+        float: left;
+        margin-right: 35px;
+        padding-left: 33px;
+      }
+
+      &.right {
+        float: right;
+        margin-left: 35px;
+        padding-right: 33px;
+      }
+    }
+
+    img {
+      width: 100%;
+      border-radius: 4px;
+    }
+
+    figcaption {
+      padding-top: 10px;
+      margin-top: 12px;
+      margin-left: 17px;
+      margin-right: 17px;
+      color: rgba(#000, 0.66);
+      font-size: 13px;
+      line-height: 1.5;
+      letter-spacing: 1px;
+      border-top: 1px solid #979797;
+      @include media-breakpoint-up(lg) {
+        margin-top: 16px;
+        margin-left: 33px;
+        margin-right: 33px;
+      }
+    }
+  }
+
+  .quote {
+    max-width: 510px;
+    margin: 30px auto;
+  }
+
+  .quote-icon-wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 11px;
+
+    &::before,
+    &::after {
+      content: '';
+      width: 100%;
+      height: 1px;
+      background-color: $quote-color;
+    }
+
+    svg {
+      width: 20px;
+      height: auto;
+      flex-shrink: 0;
+      margin-left: 33px;
+      margin-right: 33px;
+    }
+  }
+
+  blockquote {
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 1.78;
+    color: $quote-color;
+    text-align: justify;
+  }
+}
+</style>
--- components/culture-post/UiArticleBody.vue
+++ components/culture-post/UiArticleBody.vue
@@ -4,85 +4,18 @@
       <p v-for="item in brief" :key="item.id">{{ item.content }}</p>
     </div>
 
-    <!--
-      TODO: 改用更好的 render 方式，理由請見：https://vuejs.org/v2/style-guide/#Avoid-v-if-with-v-for-essential
-    -->
-    <template v-for="item in content">
-      <!-- eslint-disable vue/no-v-html -->
-      <h2
-        v-if="item.type === 'header-one' && gainContent(item)"
-        :id="`header-${item.id}`"
-        :key="item.id"
-        v-html="gainContent(item)"
-      />
-
-      <h3
-        v-else-if="item.type === 'header-two' && gainContent(item)"
-        :key="item.id"
-        v-html="gainContent(item)"
-      />
-      <!-- eslint-enable vue/no-v-html -->
-
-      <figure
-        v-else-if="item.type === 'image'"
-        :key="item.id"
-        :class="gainAlignment(item)"
-      >
-        <img v-lazy="gainImgSrc(item)" alt="" />
-
-        <figcaption v-if="gainImgCaption(item)">
-          {{ gainImgCaption(item) }}
-        </figcaption>
-      </figure>
-
-      <!-- eslint-disable vue/no-v-html -->
-      <div
-        v-else-if="item.type === 'blockquote' && gainContent(item)"
-        :key="item.id"
-        class="quote"
-      >
-        <div class="quote-icon-wrapper">
-          <SvgQuoteIcon />
-        </div>
-
-        <blockquote v-html="gainContent(item)"></blockquote>
-      </div>
-      <!-- eslint-enable vue/no-v-html -->
-
-      <ContainerParagraphWithAnnotation
-        v-else-if="item.type === 'annotation' && gainContent(item)"
-        v-slot="{ data }"
-        :key="item.id"
-        :content="gainContent(item)"
-      >
-        <UiAnnotation :key="data.id" :content="data" />
-      </ContainerParagraphWithAnnotation>
-
-      <!-- eslint-disable vue/no-v-html -->
-      <p
-        v-else-if="item.type === 'unstyled'"
-        :key="item.id"
-        v-html="gainContent(item)"
-      />
-      <!-- eslint-enable vue/no-v-html -->
-    </template>
+    <ContentHandler v-for="item in content" :key="item.id" :item="item" />
   </article>
 </template>
 
 <script>
-import ContainerParagraphWithAnnotation from '~/components/ContainerParagraphWithAnnotation.vue'
-import UiAnnotation from '~/components/culture-post/UiAnnotation.vue'
-
-import SvgQuoteIcon from '~/assets/quote.svg?inline'
+import ContentHandler from './ContentHandler.vue'
 
 export default {
   name: 'UiArticleBody',
 
   components: {
-    UiAnnotation,
-    ContainerParagraphWithAnnotation,
-
-    SvgQuoteIcon,
+    ContentHandler,
   },
 
   props: {
@@ -103,29 +36,10 @@ export default {
       return this.brief.length > 0
     },
   },
-
-  methods: {
-    gainContent(item = {}) {
-      return item.content?.[0]
-    },
-
-    gainAlignment(item = {}) {
-      // 不需要 'center' 來判斷樣式，故排除
-      return item.alignment !== 'center' && item.alignment
-    },
-    gainImgSrc(item = {}) {
-      return item.content?.[0]?.tablet?.url
-    },
-    gainImgCaption(item = {}) {
-      return item.content?.[0]?.description
-    },
-  },
 }
 </script>
 
 <style lang="scss" scoped>
-$quote-color: #4a90e2;
-
 .article-body {
   padding-bottom: 60px;
   font-size: 15px;
@@ -168,114 +82,6 @@ $quote-color: #4a90e2;
     p + p {
       margin-top: 2em;
     }
-  }
-
-  h2,
-  h3 {
-    font-family: source-han-serif-tc, 'Songti TC', serif;
-    font-weight: 700;
-    line-height: 1.5;
-    margin-top: 60px;
-  }
-
-  h2 {
-    font-size: 28px;
-    @include media-breakpoint-up(lg) {
-      font-size: 30px;
-    }
-  }
-
-  h3 {
-    font-size: 18px;
-  }
-
-  figure {
-    width: 100%;
-    padding-left: 0;
-    padding-right: 0;
-    @include media-breakpoint-up(md) {
-      &.left,
-      &.right {
-        clear: both;
-        width: 300px;
-        margin-bottom: 35px;
-
-        figcaption {
-          margin-left: 0;
-          margin-right: 0;
-        }
-      }
-
-      &.left {
-        float: left;
-        margin-right: 35px;
-        padding-left: 33px;
-      }
-
-      &.right {
-        float: right;
-        margin-left: 35px;
-        padding-right: 33px;
-      }
-    }
-
-    img {
-      width: 100%;
-      border-radius: 4px;
-    }
-
-    figcaption {
-      padding-top: 10px;
-      margin-top: 12px;
-      margin-left: 17px;
-      margin-right: 17px;
-      color: rgba(#000, 0.66);
-      font-size: 13px;
-      line-height: 1.5;
-      letter-spacing: 1px;
-      border-top: 1px solid #979797;
-      @include media-breakpoint-up(lg) {
-        margin-top: 16px;
-        margin-left: 33px;
-        margin-right: 33px;
-      }
-    }
-  }
-
-  .quote {
-    max-width: 510px;
-    margin: 30px auto;
-  }
-
-  .quote-icon-wrapper {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 11px;
-
-    &::before,
-    &::after {
-      content: '';
-      width: 100%;
-      height: 1px;
-      background-color: $quote-color;
-    }
-
-    svg {
-      width: 20px;
-      height: auto;
-      flex-shrink: 0;
-      margin-left: 33px;
-      margin-right: 33px;
-    }
-  }
-
-  blockquote {
-    font-size: 18px;
-    font-weight: 500;
-    line-height: 1.78;
-    color: $quote-color;
-    text-align: justify;
   }
 }
 </style>
