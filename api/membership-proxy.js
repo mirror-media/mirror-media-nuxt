999f99e08f3940cc7c61b4d7bec922e1acba02df
kwhsiung <kaiwenhsiung@gmail.com>
2020-12-10 12:40:52 +0800

    refactor(api): move proxy behaviors to a create proxy function

---
+++ api/membership-proxy.js
@@ -0,0 +1,11 @@
+const {
+  API_PROTOCOL,
+  API_HOST_MEMBERSHIP_GATEWAY,
+  API_PATH_MEMBERSHIP_GATEWAY,
+  API_PORT_MEMBERSHIP_GATEWAY,
+} = require('../configs/config')
+const { createProxy } = require('./helpers')
+
+module.exports = createProxy(
+  `${API_PROTOCOL}://${API_HOST_MEMBERSHIP_GATEWAY}:${API_PORT_MEMBERSHIP_GATEWAY}${API_PATH_MEMBERSHIP_GATEWAY}`
+)
