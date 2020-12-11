98ea7f92248431c3ece72b03951b6b5be9843726
HY Tan <hsuehyungtan@gmail.com>
2020-12-11 12:21:50 +0800

    test(404): add 404 page test

---
+++ layouts/__tests__/error.spec.js
@@ -0,0 +1,38 @@
+import Page from '../error.vue'
+import UiArticleCard from '../../components/UiArticleCard.vue'
+
+import createWrapperHelper from '~/test/helpers/createWrapperHelper'
+
+const createWrapper = createWrapperHelper()
+
+describe('page information', () => {
+  const wrapper = createWrapper(Page)
+  test('Should have proper message', () => {
+    expect(wrapper.find('h1').text()).toBe('404')
+    expect(wrapper.find('h2').text()).toContain('抱歉！')
+    expect(wrapper.find('h2').text()).toContain('找不到這個網址')
+  })
+  test('Should have link to home page', () => {
+    const link = wrapper.find('a')
+    expect(link.attributes().href).toBe('/')
+    expect(link.text()).toBe('| 回首頁 |')
+  })
+})
+
+describe('latest stories', () => {
+  const mockItems = [{}]
+  const wrapper = createWrapper(Page, {
+    data() {
+      return {
+        latestStories: mockItems,
+      }
+    },
+  })
+  test('Should have proper heading', () => {
+    expect(wrapper.html()).toContain('熱門文章')
+  })
+  test('Should have UiArticleCard', () => {
+    const items = wrapper.findAllComponents(UiArticleCard)
+    expect(items).toHaveLength(mockItems.length)
+  })
+})
