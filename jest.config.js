cef0360b4b98ce07dfb1f812b80925cb5a1c0b4f
kwhsiung <kaiwenhsiung@gmail.com>
2020-12-21 18:00:46 +0800

    test: set up graphql transform

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest',
+    '\\.(gql|graphql)$': 'jest-transform-graphql',
     '\\.(scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'jest-transform-stub',
   },
