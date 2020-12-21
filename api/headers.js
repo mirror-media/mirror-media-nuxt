633bb14db6d6129c04ab2172bd939865fe10ff0e
yeefun <szyeefun@gmail.com>
2020-12-21 17:14:16 +0800

    feat: change Cache-Control of home

--- api/headers.js
+++ api/headers.js
@@ -6,7 +6,7 @@ module.exports = function handleHeaders(req, res, next) {
   if (
     ['local', 'dev'].includes(ENV) ||
     /keystone/gs.test(hostname) ||
-    /^\/$|^\/(video_category|externals)\//gs.test(url) ||
+    /^\/(video_category|externals)\//gs.test(url) ||
     new RegExp(PREVIEW_QUERY, 'gs').test(url)
   ) {
     res.setHeader('Cache-Control', 'no-store')
@@ -14,18 +14,16 @@ module.exports = function handleHeaders(req, res, next) {
     return next()
   }
 
-  if (/^\/(api|story|culture-post|video|app)\//gs.test(url)) {
-    res.setHeader('Cache-Control', `private, max-age=${60 * 10}`)
-
-    return next()
-  }
-
-  const isDefaultListingPages = /^\/$|^\/(section|category|topic|search|author|tag)\//gs.test(
+  const isDefaultListingPages = /^\/(section|category|topic|search|author|tag)\//gs.test(
     url
   )
   if (isDefaultListingPages) {
     res.setHeader('Cache-Control', `private, max-age=${60 * 5}`)
+
+    return next()
   }
+
+  res.setHeader('Cache-Control', `private, max-age=${60 * 10}`)
 
   return next()
 }
