0c9af3eee98e5441fd0518e17f4faa33e3851de4
yeefun <szyeefun@gmail.com>
2020-12-21 16:47:09 +0800

    refactor(home): clarify the tests of the UiArticleListFocus

--- components/__tests__/UiArticleListFocus.spec.js
+++ components/__tests__/UiArticleListFocus.spec.js
@@ -1,57 +1,76 @@
+import flushPromises from 'flush-promises'
+
 import UiArticleListFocus from '../UiArticleListFocus.vue'
 
 import createWrapperHelper from '~/test/helpers/createWrapperHelper'
 
-const mockAricleMain = {
-  title: '圓佛洛伊德遺願',
-  href: '/test-href-main/',
-  imgSrc: 'test-src',
-}
-const mockArticlesRelated = {
-  slug: '20200603edi031',
-  title: '洛杉磯面目全非',
-  href: '/test-href-related/',
-}
-
 const createWrapper = createWrapperHelper({
   propsData: {
-    articleMain: mockAricleMain,
-    articlesRelated: [mockArticlesRelated],
+    articleMain: {},
+    articlesRelated: [],
   },
 })
 
-describe('main article', () => {
-  test('render the proper content', () => {
-    const wrapper = createWrapper(UiArticleListFocus)
+const articleMainMock = {
+  title: 'test-title-main',
+  href: '/test-href-main/',
+  imgSrc: 'test-src',
+}
+const articlesRelated1Mock = {
+  slug: 'test-slug-1',
+  title: 'test-title-related-1',
+  href: '/test-href-related-1/',
+}
+const articlesRelated2Mock = {
+  slug: 'test-slug-2',
+  title: 'test-title-related-2',
+  href: '/test-href-related-2/',
+}
 
-    const mainLink = wrapper.get(`[href="${mockAricleMain.href}"]`)
+test('display the articles', async function () {
+  expect.assertions(7)
 
-    // expect(mainLink.get('img').attributes().src).toBe(
-    //   mockAricleMain.heroImage.image.resizedTargets.mobile.url
-    // )
-    expect(mainLink.get('h1').text()).toBe(mockAricleMain.title)
+  /* Arrange */
+  const sut = createWrapper(UiArticleListFocus, {
+    propsData: {
+      articleMain: articleMainMock,
+      articlesRelated: [articlesRelated1Mock, articlesRelated2Mock],
+    },
   })
+
+  // for v-lazy
+  await flushPromises()
+
+  /* Assert */
+  expect(sut.get('img').attributes()['data-src']).toBe(articleMainMock.imgSrc)
+
+  const links = sut.findAll('a')
+  expect(links.at(0).attributes().href).toBe(articleMainMock.href)
+  expect(links.at(1).attributes().href).toBe(articlesRelated1Mock.href)
+  expect(links.at(2).attributes().href).toBe(articlesRelated2Mock.href)
+
+  const titles = sut.findAll('h1')
+  expect(titles.at(0).text()).toBe(articleMainMock.title)
+  expect(titles.at(1).text()).toBe(articlesRelated1Mock.title)
+  expect(titles.at(2).text()).toBe(articlesRelated2Mock.title)
 })
 
-describe('related articles', () => {
-  test('render the proper content', () => {
-    const wrapper = createWrapper(UiArticleListFocus)
-
-    const relatedLink = wrapper.get(`[href="${mockArticlesRelated.href}"]`)
-    expect(relatedLink.get('h1').text()).toBe(mockArticlesRelated.title)
+test('send the GA event when users click the link of an article', function () {
+  /* Arrange */
+  const sut = createWrapper(UiArticleListFocus, {
+    propsData: {
+      articleMain: articleMainMock,
+      articlesRelated: [articlesRelated1Mock],
+    },
   })
-})
 
-describe('emitGa method', () => {
-  test('with a proper argument when users click an article link', () => {
-    const wrapper = createWrapper(UiArticleListFocus)
+  const links = sut.findAll('a')
 
-    const mainLink = wrapper.get(`[href="${mockAricleMain.href}"]`)
-    mainLink.trigger('click')
-    expect(wrapper.emitted().sendGa[0]).toBeTruthy()
+  for (let i = 0; i < links.length; i += 1) {
+    /* Act */
+    links.at(i).trigger('click')
 
-    const relatedLink = wrapper.get(`[href="${mockArticlesRelated.href}"]`)
-    relatedLink.trigger('click')
-    expect(wrapper.emitted().sendGa[1]).toBeTruthy()
-  })
+    /* Assert */
+    expect(sut.emitted().sendGa[i]).toBeTruthy()
+  }
 })
