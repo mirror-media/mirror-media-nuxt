c98ed2aff11d5cbc2cee2d104716b710aff32af7
yeefun <szyeefun@gmail.com>
2020-12-23 16:51:10 +0800

    feat(membership): add the banner & intro in the login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,49 +1,89 @@
 <template>
-  <section class="page">
-    <div v-if="pageState === 'form'" class="form-wrapper">
-      <h1 class="form-wrapper__title title">登入</h1>
-      <div class="form-wrapper__federated-login federated-login">
-        <div class="facebook-login-wrapper">
-          <div
-            v-show="isFederatedRedirectResultLoading"
-            class="federated-login-loading-wrapper"
-          >
-            <UiMembershipSpinner />
-          </div>
-          <ContainerMembershipLoginWithFacebook
-            v-show="!isFederatedRedirectResultLoading"
-          />
-        </div>
-        <div class="federated-login__login-wrapper google-login-wrapper">
-          <div
-            v-show="isFederatedRedirectResultLoading"
-            class="federated-login-loading-wrapper"
-          >
-            <UiMembershipSpinner />
-          </div>
-          <ContainerMembershipLoginWithGoogle
-            v-show="!isFederatedRedirectResultLoading"
-          />
-        </div>
+  <section class="login">
+    <picture class="banner">
+      <source
+        media="(min-width: 1200px)"
+        type="image/webp"
+        srcset="~/assets/membership-banner-login-xl.webp"
+      />
+      <source
+        media="(min-width: 1200px)"
+        type="image/jpeg"
+        srcset="~/assets/membership-banner-login-xl.jpg"
+      />
+
+      <source
+        media="(min-width: 768px)"
+        type="image/webp"
+        srcset="~/assets/membership-banner-login-md.webp"
+      />
+      <source
+        media="(min-width: 768px)"
+        type="image/jpeg"
+        srcset="~/assets/membership-banner-login-md.jpg"
+      />
+
+      <source
+        type="image/webp"
+        srcset="~/assets/membership-banner-login.webp"
+      />
+      <img src="~/assets/membership-banner-login.jpg" alt="" />
+    </picture>
+
+    <div class="container">
+      <div class="intro">
+        <h1>歡迎加入鏡週刊會員訂閱服務</h1>
+        <p>
+          在資訊爆炸與碎片的年代，新聞更需回歸純粹，讓閱讀更清澈、真實與深度。因此我們秉持明鏡之心，充份反映事實，反映時代與反映人性，持續挖掘有料內容，以一手原創題材、深度調查報導為宗旨，讓閱聽大眾獲取最完整的資訊。
+        </p>
+        <p>現在，請立即加入，成為我們的會員，體驗有質量、無干擾的新聞閱讀。</p>
       </div>
-      <div class="form-wrapper__separator separator">
-        <p>或</p>
+
+      <div v-if="pageState === 'form'" class="form-wrapper">
+        <h2 class="form-wrapper__title title">登入</h2>
+        <div class="form-wrapper__federated-login federated-login">
+          <div class="facebook-login-wrapper">
+            <div
+              v-show="isFederatedRedirectResultLoading"
+              class="federated-login-loading-wrapper"
+            >
+              <UiMembershipSpinner />
+            </div>
+            <ContainerMembershipLoginWithFacebook
+              v-show="!isFederatedRedirectResultLoading"
+            />
+          </div>
+          <div class="federated-login__login-wrapper google-login-wrapper">
+            <div
+              v-show="isFederatedRedirectResultLoading"
+              class="federated-login-loading-wrapper"
+            >
+              <UiMembershipSpinner />
+            </div>
+            <ContainerMembershipLoginWithGoogle
+              v-show="!isFederatedRedirectResultLoading"
+            />
+          </div>
+        </div>
+        <div class="form-wrapper__separator separator">
+          <p>或</p>
+        </div>
+        <ContainerMembershipLoginWithEmail
+          @success="handleEmailLoginSuccess"
+          @error="handleLoginError"
+        />
       </div>
-      <ContainerMembershipLoginWithEmail
-        @success="handleEmailLoginSuccess"
-        @error="handleLoginError"
+      <UiMembershipEmailSuccess
+        v-else-if="pageState === 'success'"
+        class="success-wrapper"
+        :emailInput="emailShowInSuccess"
+      />
+      <UiMembershipError
+        v-else-if="pageState === 'error'"
+        class="error-wrapper"
+        @backToForm="handleBackToForm"
       />
     </div>
-    <UiMembershipEmailSuccess
-      v-else-if="pageState === 'success'"
-      class="success-wrapper"
-      :emailInput="emailShowInSuccess"
-    />
-    <UiMembershipError
-      v-else-if="pageState === 'error'"
-      class="error-wrapper"
-      @backToForm="handleBackToForm"
-    />
   </section>
 </template>
 
@@ -124,17 +164,84 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.page {
+.login {
+  padding-bottom: 24px;
+  @include media-breakpoint-up(md) {
+    padding-bottom: 48px;
+    background-color: #f2f2f2;
+  }
+  @include media-breakpoint-up(xl) {
+    padding-bottom: 60px;
+  }
+}
+
+.banner {
+  display: block;
+  margin-bottom: 48px;
+  @include media-breakpoint-up(xl) {
+    margin-bottom: 60px;
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+}
+
+.container {
   padding: 0 20px;
-  min-height: calc(100vh - 118px);
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
+}
+
+$font-size--intro: 18px;
+$font-size--intro--xl: 20px;
+$line-height--intro: 1.6;
+
+.intro {
+  color: #4a4a4a;
+  font-size: $font-size--intro;
+  line-height: $line-height--intro;
+  margin-bottom: 24px;
+  max-width: 608px;
+  @include media-breakpoint-up(md) {
+    margin-bottom: 48px;
+  }
+  @include media-breakpoint-up(xl) {
+    font-size: $font-size--intro--xl;
+    margin-bottom: 60px;
+    max-width: 875px;
+  }
+
+  p + p {
+    margin-top: $font-size--intro * $line-height--intro;
+    @include media-breakpoint-up(xl) {
+      margin-top: $font-size--intro--xl * $line-height--intro;
+    }
+  }
+}
+
+h1 {
+  font-size: 28px;
+  line-height: 1.46;
+  letter-spacing: 1px;
+  text-align: center;
+  margin-bottom: 16px;
+  @include media-breakpoint-up(md) {
+    margin-bottom: 20px;
+  }
+  @include media-breakpoint-up(xl) {
+    font-size: 32px;
+    letter-spacing: 1.2px;
+    margin-bottom: 32px;
+  }
 }
 
 .form-wrapper {
   width: 100%;
+  max-width: 424px;
+
   &__federated-login {
     margin: 20px 0 0 0;
   }
