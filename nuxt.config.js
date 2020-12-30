26fafebcff61bf0fb7a85b28e1ac02bf11bd1490
kwhsiung <kaiwenhsiung@gmail.com>
2020-12-30 14:54:37 +0800

    feat: add premium back to redis cache

--- nuxt.config.js
+++ nuxt.config.js
@@ -332,7 +332,7 @@ module.exports = {
 
     key(route) {
       // We should configure cache pages path right here.
-      const ignorePages = /^(?!\/login|\/profile|\/finishSignUp|\/cancelMembership|\/premium).+/
+      const ignorePages = /^(?!\/login|\/profile|\/finishSignUp|\/cancelMembership).+/
       const cachePages = [ignorePages]
 
       const shouldCacheCurrentRoute = cachePages.some((pat) =>
