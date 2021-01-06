b970d2a6db2528b3c82783d1f01a28f6bc600183
HY Tan <hsuehyungtan@gmail.com>
2021-01-06 17:19:08 +0800

    feat: add comScore script

--- nuxt.config.js
+++ nuxt.config.js
@@ -133,12 +133,19 @@ module.exports = {
       },
     ],
     script: [
-      // likr 禾多推播
       ...(ENV !== 'lighthouse'
         ? [
+            // likr 禾多推播
             {
               hid: 'likrNotification',
               innerHTML: `window.AviviD = window.AviviD || {settings:{},status:{}}; AviviD.web_id = "mirrormedia"; AviviD.category_id = "20180905000003"; AviviD.tracking_platform = 'likr'; (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl+'&timestamp='+new Date().getTime();f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-W9F4QDN'); (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl+'&timestamp='+new Date().getTime();f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-MKB8VFG');`,
+            },
+
+            // comScore
+            {
+              hid: 'comScore',
+              src: 'https://sb.scorecardresearch.com/beacon.js',
+              async: true,
             },
           ]
         : []),
