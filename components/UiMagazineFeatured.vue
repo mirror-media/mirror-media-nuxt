93abb2af32462d18436d21e668e978f7901ad219
kwhsiung <kaiwenhsiung@gmail.com>
2021-01-18 10:40:21 +0800

    feat: add magazine page and current featured magazine component
    feat: add title of magazine page
    
    feat: add UiMagazineFeatured
    
    feat(UiMagazineFeatured): add coverImgUrl props
    
    feat(UiMagazineFeatured): add label props
    
    feat(UiMagazineFeatured): add title props
    
    feat(UiMagazineFeatured): add downloadLink props
    
    feat(UiMagazineFeatured): add descriptions props
    
    refactor(UiMagazineFeatured): transform arrow function to named function
    
    feat(UiMagazineFeatured): open a new tab after click the download link
    
    feat(UiMagazineFeatured): add wording of download link
    
    fix(UiMagazineFeatured): add index to the key of the paragraphs
    
    feat(UiMagazineFeatured): refine template and add styles
    
    feat: add current featured magazine and adjust styles of magazine page

---
+++ components/UiMagazineFeatured.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="featured">
+    <div class="featured__cover-wrapper cover-wrapper">
+      <img class="cover-img" :src="coverImgUrl" alt="magazine-cover-img" />
+      <a
+        :href="downloadLink"
+        target="_blank"
+        class="cover-wrapper__download-link download-link desktop"
+      >
+        立即下載
+      </a>
+    </div>
+    <div class="featured__info-wrapper info-wrapper">
+      <p class="label" v-text="label" />
+      <h1 class="info-wrapper__title title" v-text="title" />
+      <div
+        class="info-wrapper__description-wrapper description-wrapper desktop"
+      >
+        <p
+          v-for="(description, i) in descriptions"
+          :key="`${description}-${i}`"
+          class="description"
+          v-text="description"
+        />
+      </div>
+      <a
+        :href="downloadLink"
+        target="_blank"
+        class="info-wrapper__download-link download-link mobile"
+      >
+        立即下載
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    coverImgUrl: {
+      type: String,
+      default: '',
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    descriptions: {
+      type: Array,
+      default: () => [],
+    },
+    downloadLink: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.featured {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  &__info-wrapper {
+    margin: 10px 0 0 0;
+    @include media-breakpoint-up(md) {
+      margin: 0 0 0 30px;
+    }
+  }
+  @include media-breakpoint-up(md) {
+    width: 672px;
+    flex-direction: row;
+    align-items: flex-start;
+  }
+}
+
+.cover-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-width: 160px;
+  width: 160px;
+  &__download-link {
+    margin: 16px 0 0 0;
+  }
+}
+
+.cover-img {
+  width: 100%;
+  height: 212px;
+  object-fit: cover;
+}
+
+.info-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  &__title {
+    margin: 10px 0 0 0;
+  }
+  &__description-wrapper {
+    margin: 16px 0 0 0;
+  }
+  &__download-link {
+    margin: 10px 0 0 0;
+  }
+  @include media-breakpoint-up(md) {
+    align-items: flex-start;
+  }
+}
+
+.label {
+  font-size: 16px;
+  color: #1d9fb8;
+}
+
+.title {
+  font-size: 22px;
+  font-weight: 400;
+  color: #4a4a4a;
+  text-align: center;
+  @include media-breakpoint-up(md) {
+    text-align: left;
+  }
+}
+
+.description-wrapper {
+  flex-direction: column;
+}
+
+.description {
+  font-size: 16px;
+  color: #9b9b9b;
+  & + & {
+    margin: 16px 0 0 0;
+  }
+}
+
+.download-link {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  width: 100%;
+  height: 48px;
+  background-color: #1d9fb8;
+  border-radius: 2px;
+}
+
+.mobile {
+  display: flex;
+  @include media-breakpoint-up(md) {
+    display: none;
+  }
+}
+.desktop {
+  display: none;
+  @include media-breakpoint-up(md) {
+    display: flex;
+  }
+}
+</style>
