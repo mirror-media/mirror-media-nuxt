c72d580865878eec569bef98c9db97cf7aff9530
HY Tan <hsuehyungtan@gmail.com>
2021-01-29 16:58:14 +0800

    feat: add Alexa script and update comScore script

--- nuxt.config.js
+++ nuxt.config.js
@@ -145,17 +145,39 @@ module.exports = {
               innerHTML: `window.AviviD = window.AviviD || {settings:{},status:{}}; AviviD.web_id = "mirrormedia"; AviviD.category_id = "20180905000003"; AviviD.tracking_platform = 'likr'; (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl+'&timestamp='+new Date().getTime();f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-W9F4QDN'); (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl+'&timestamp='+new Date().getTime();f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-MKB8VFG');`,
             },
 
-            // comScore
+            // comScore Tag
             {
               hid: 'comScore',
-              src: 'https://sb.scorecardresearch.com/beacon.js',
-              async: true,
+              innerHTML: `
+                var _comscore = _comscore || [ ];
+                _comscore.push({ c1: "2", c2: "24318560",
+                options: {
+                url_append: "comscorekw=keyword"
+                }
+                });
+                (function() {
+                var s = document.createElement("script"), el =
+                document.getElementsByTagName("script")[0]; s.async = true;
+                s.src = (document.location.protocol == "https:" ? "https://sb" : "http://b") +
+                ".scorecardresearch.com/beacon.js";
+                el.parentNode.insertBefore(s, el);
+                })();
+              `,
             },
 
             // https://support.google.com/optimize/answer/7513085
             {
               hid: 'googleOptimize',
               src: `https://www.googleoptimize.com/optimize.js?id=${GOOGLE_OPT_CONTAINER_ID}`,
+            },
+
+            // Alexa Certify Javascript
+            {
+              hid: 'alexa',
+              innerHTML: `
+                _atrk_opts = { atrk_acct:"JEZvr1zDGU20kU", domain:"mirrormedia.mg",dynamic: true};
+                (function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://certify-js.alexametrics.com/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+              `,
             },
           ]
         : []),
@@ -167,7 +189,25 @@ module.exports = {
         async: true,
       },
     ],
+    noscript: [
+      // Alexa Certify Javascript
+      {
+        hid: 'alexaNoScript',
+        innerHTML:
+          '<img src="https://certify.alexametrics.com/atrk.gif?account=JEZvr1zDGU20kU" style="display:none" height="1" width="1" alt="" />',
+      },
+
+      // comScore Tag
+      {
+        hid: 'comScoreNoScript',
+        innerHTML: `<img src="http://b.scorecardresearch.com/p?c1=2&c2=&cv=2.0&cj=1&comscorekw=keyword" />`,
+      },
+    ],
     __dangerouslyDisableSanitizersByTagID: {
+      alexa: ['innerHTML'],
+      alexaNoScript: ['innerHTML'],
+      comScore: ['innerHTML'],
+      comScoreNoScript: ['innerHTML'],
       likrNotification: ['innerHTML'],
     },
   },
