aa966ae1cafb3ded43ea10a5bf5b336eb691f049
yeefun <szyeefun@gmail.com>
2021-02-09 20:24:52 +0800

    refactor(list): let the page of section.topic use the list mixin

--- pages/section/topic.vue
+++ pages/section/topic.vue
@@ -6,7 +6,7 @@
       class="section__list"
       :listTitle="'Topic'"
       :listTitleColor="'#BCBCBC'"
-      :listData="listDataFirstPage"
+      :listData="listItemsInFirstPage"
     >
       <template v-for="unit in microAdUnits" v-slot:[unit.name]>
         <MicroAd :key="unit.name" :unitId="unit.id" />
@@ -16,9 +16,9 @@
     <ContainerGptAd class="section__ad" pageKey="other" adKey="FT" />
 
     <UiArticleList
-      v-show="showListDataLoadmorePage"
+      v-if="shouldMountLoadmoreList"
       class="section__list"
-      :listData="listDataLoadmorePage"
+      :listData="listItemsInLoadmorePage"
     />
     <UiInfiniteLoading @infinite="infiniteHandler" />
 
@@ -28,16 +28,20 @@
 </template>
 
 <script>
-import _ from 'lodash'
 import UiArticleList from '~/components/UiArticleList.vue'
 import UiInfiniteLoading from '~/components/UiInfiniteLoading.vue'
 import ContainerGptAd from '~/components/ContainerGptAd.vue'
 import ContainerFullScreenAds from '~/components/ContainerFullScreenAds.vue'
 import UiStickyAd from '~/components/UiStickyAd.vue'
 import MicroAd from '~/components/MicroAd.vue'
+
+import { processTwoLists } from '~/mixins/list.js'
+
 import { SITE_TITLE, SITE_URL } from '~/constants'
 import { MICRO_AD_UNITS } from '~/constants/ads.js'
 import { stripHtmlTags } from '~/utils/article.js'
+
+const LIST_MAX_RESULTS = 9
 
 export default {
   name: 'SectionTopic',
@@ -49,93 +53,40 @@ export default {
     UiStickyAd,
     MicroAd,
   },
+
+  mixins: [
+    processTwoLists({
+      maxResults: LIST_MAX_RESULTS,
+
+      async fetchList(page) {
+        return await this.$fetchTopics({
+          maxResults: LIST_MAX_RESULTS,
+          page,
+        })
+      },
+
+      transformListItemContent(item = {}) {
+        return {
+          href: item.id ? `/topic/${item.id}` : '/',
+          imgSrc: item.ogImage?.image?.resizedTargets?.mobile?.url,
+          infoTitle: item.name ?? '',
+          infoDescription:
+            stripHtmlTags(item.brief?.html ?? '') || (item.ogDescription ?? ''),
+        }
+      },
+    }),
+  ],
+
   async fetch() {
-    const response = await this.fetchTopicsListing({ page: 1 })
-    this.setListData(response)
-    this.setListDataTotal(response)
-    this.listDataCurrentPage += 1
+    await this.initList()
   },
+
   data() {
     return {
-      listData_: [],
-      listDataCurrentPage: 0,
-      listDataMaxResults: 9,
-      listDataTotal: undefined,
       microAdUnits: MICRO_AD_UNITS.LISTING.RWD,
     }
   },
-  computed: {
-    listDataPageLimit() {
-      if (this.listDataTotal === undefined) {
-        return undefined
-      }
-      return Math.ceil(this.listDataTotal / this.listDataMaxResults)
-    },
 
-    listData() {
-      return _.uniqBy(this.listData_, function identifyDuplicatedItemById(
-        listItem
-      ) {
-        return listItem.id
-      })
-    },
-    listDataFirstPage() {
-      return this.listData.slice(0, this.listDataMaxResults)
-    },
-    listDataLoadmorePage() {
-      return this.listData.slice(this.listDataMaxResults, Infinity)
-    },
-    showListDataLoadmorePage() {
-      return this.listDataLoadmorePage.length > 0
-    },
-  },
-  methods: {
-    mapDataToComponentProps(item) {
-      return {
-        id: item.id,
-        href: item.id ? `/topic/${item.id}` : '/',
-        imgSrc: item.ogImage?.image?.resizedTargets?.mobile?.url,
-        imgText: undefined,
-        imgTextBackgroundColor: undefined,
-        infoTitle: item.name ?? '',
-        infoDescription: item.brief?.html
-          ? stripHtmlTags(item.brief?.html)
-          : item.ogDescription,
-      }
-    },
-    async fetchTopicsListing({ page = 1 } = {}) {
-      const response = await this.$fetchTopics({
-        maxResults: this.listDataMaxResults,
-        page,
-      })
-      return response
-    },
-    setListData(response = {}) {
-      let listData = response.items ?? []
-      listData = listData.map(this.mapDataToComponentProps)
-      this.listData_.push(...listData)
-    },
-    setListDataTotal(response = {}) {
-      this.listDataTotal = response.meta?.total ?? 0
-    },
-    async infiniteHandler($state) {
-      this.listDataCurrentPage += 1
-      try {
-        const response = await this.fetchTopicsListing({
-          page: this.listDataCurrentPage,
-        })
-        this.setListData(response)
-
-        if (this.listDataCurrentPage >= this.listDataPageLimit) {
-          $state.complete()
-        } else {
-          $state.loaded()
-        }
-      } catch (e) {
-        $state.error()
-      }
-    },
-  },
   head() {
     const title = `Topic - ${SITE_TITLE}`
     return {
