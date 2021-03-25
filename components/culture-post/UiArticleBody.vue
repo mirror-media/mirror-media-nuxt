4fa89ac1327fb746a2e28e98705a13fc56fa3b64
KJ WEN <kjwen310@gmail.com>
2021-03-25 17:24:12 +0800

    feat: add magazine button to wide page (#299)
    * feat: add magazine button to wide page
    
    * fix: fix the rederict problem
    
    * refactor: move the magazine rederiction section from ContainerCulturePost to UiAriticle

--- components/culture-post/UiArticleBody.vue
+++ components/culture-post/UiArticleBody.vue
@@ -6,6 +6,11 @@
     </div>
 
     <ContentHandler v-for="item in content" :key="item.id" :item="item" />
+
+    <div class="magazine">
+      <div>下載鏡週刊電子雜誌</div>
+      <button type="button" @click="enterMagazinePage">立即下載</button>
+    </div>
   </article>
 </template>
 
@@ -35,6 +40,11 @@ export default {
   computed: {
     doesHaveBrief() {
       return this.brief.length > 0
+    },
+  },
+  methods: {
+    enterMagazinePage() {
+      window.location.href = '/magazine/'
     },
   },
 }
@@ -98,6 +108,43 @@ export default {
 
     p + p {
       margin-top: 2em;
+    }
+  }
+}
+
+.magazine {
+  margin: 24px 27px;
+  font-size: 24px;
+  line-height: 36px;
+  text-align: center;
+  background-color: #fff;
+  padding: 20px;
+  color: #054f77;
+  border: 1px solid rgba(155, 155, 155, 0.2);
+  box-shadow: 0px 0px 20px -4px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  @include media-breakpoint-up(xl) {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 24px 32px;
+    font-size: 20px;
+  }
+
+  button {
+    width: 100%;
+    background-color: #1d9fb8;
+    border-radius: 2px;
+    line-height: 1.38;
+    color: #fff;
+    padding: 12px 4px;
+    margin-top: 16px;
+    font-size: 18px;
+    line-height: 25px;
+    @include media-breakpoint-up(xl) {
+      margin-top: 0;
+      max-width: 170px;
+      line-height: 27px;
     }
   }
 }
