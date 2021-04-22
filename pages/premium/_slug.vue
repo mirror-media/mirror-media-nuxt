cfc10386274413bf7ed85315ccea1c86ea3335cc
kwhsiung <kaiwenhsiung@gmail.com>
2021-04-22 18:08:33 +0800

    fix: bring fetchPostFromMembershipGateway back

--- pages/premium/_slug.vue
+++ pages/premium/_slug.vue
@@ -23,7 +23,7 @@ export default {
   },
   async fetch() {
     const [postResponse] = await Promise.allSettled([
-      this.$fetchPosts(
+      this.$fetchPostsFromMembershipGateway(
         {
           slug: this.storySlug,
           isAudioSiteOnly: false,
