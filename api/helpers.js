f260e8f5b904d3847e5e0e6c1afbba1f4681a24b
kwhsiung <kaiwenhsiung@gmail.com>
2021-05-03 11:57:53 +0800

    revert: disable /api/v2/search

--- api/helpers.js
+++ api/helpers.js
@@ -3,15 +3,6 @@ const { API_TIMEOUT } = require('../configs/config')
 
 function createProxy(baseUrl) {
   return async function (req, res) {
-    if ((req.url || '').startsWith('/search')) {
-      console.log(
-        `[Search Log] the incoming request headers is: ${JSON.stringify(
-          req.headers
-        )}`
-      )
-      return res.status(500).end('error')
-    }
-
     const headers = Object.assign({}, req.headers)
     delete headers.host
 
@@ -21,7 +12,7 @@ function createProxy(baseUrl) {
         url: `${baseUrl}${req.url}`,
         headers,
         data: req.method === 'GET' ? undefined : req.body,
-        timeout: (req.url || '').startsWith('/search') ? 500 : API_TIMEOUT,
+        timeout: API_TIMEOUT,
       })
 
       if (response.data._status === 'ERR') {
