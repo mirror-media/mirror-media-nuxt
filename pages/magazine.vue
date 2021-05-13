2cf28a9458eee58dd51fcd1bca9d2d5301721c1b
kwhsiung <kaiwenhsiung@gmail.com>
2021-05-13 13:01:59 +0800

    feat(magazine): redesign the magazine page

--- pages/magazine.vue
+++ pages/magazine.vue
@@ -5,23 +5,31 @@
       class="page__current-featured-magazine-wrapper current-featured-magazine-wrapper"
     >
       <UiMagazineFeatured
-        :coverImgUrl="magazineFeatured.coverImgUrl"
-        :label="magazineFeatured.issue"
-        :title="magazineFeatured.title"
-        :descriptions="magazineFeatured.descriptions"
-        :downloadLink="magazineFeatured.pdfLink"
+        class="current-featured-magazine-wrapper__magazine"
+        :coverImgUrl="magazineFeatured[0].coverImgUrl"
+        :label="magazineFeatured[0].issue"
+        :title="magazineFeatured[0].title"
+        :downloadLink="magazineFeatured[0].pdfLink"
+        @downloadLinkClick="handleDownloadLinkClick"
+      />
+      <UiMagazineFeatured
+        class="current-featured-magazine-wrapper__magazine"
+        :coverImgUrl="magazineFeatured[1].coverImgUrl"
+        :label="magazineFeatured[1].issue"
+        :title="magazineFeatured[1].title"
+        :downloadLink="magazineFeatured[1].pdfLink"
         @downloadLinkClick="handleDownloadLinkClick"
       />
     </div>
     <section
       class="page__magazine-showcase-list-wrapper magazine-showcase-list-wrapper"
     >
-      <h1 class="title">所有期數</h1>
+      <h1 class="title">近期雜誌</h1>
       <ol
         class="magazine-showcase-list-wrapper__magazine-showcase-list magazine-showcase-list"
       >
         <li
-          v-for="item in magazineShowcases"
+          v-for="item in magazineCurrent"
           :key="item.id"
           class="magazine-showcase-list__list-item magazine-showcase-list-item"
         >
@@ -35,27 +43,81 @@
         </li>
       </ol>
     </section>
+    <section
+      class="page__magazine-showcase-list-wrapper magazine-showcase-list-wrapper"
+    >
+      <h1 class="title">特刊</h1>
+      <ol
+        class="magazine-showcase-list-wrapper__magazine-showcase-list magazine-showcase-list"
+      >
+        <li
+          v-for="item in magazineSpecial"
+          :key="item.id"
+          class="magazine-showcase-list__list-item magazine-showcase-list-item"
+        >
+          <UiMagazineShowcaseItem
+            :coverImgUrl="item.coverImgUrl"
+            :label="item.publishedDate"
+            :title="item.issue"
+            :downloadLink="item.pdfLink"
+            @downloadLinkClick="handleDownloadLinkClick"
+          />
+        </li>
+      </ol>
+    </section>
+    <template v-if="shouldLoadmore">
+      <UiInfiniteLoading @infinite="infiniteHandler" />
+    </template>
   </section>
 </template>
 
 <script>
-import _ from 'lodash'
 import dayjs from 'dayjs'
 import UiMagazineFeatured from '~/components/UiMagazineFeatured.vue'
 import UiMagazineShowcaseItem from '~/components/UiMagazineShowcaseItem.vue'
+import UiInfiniteLoading from '~/components/UiInfiniteLoading.vue'
+import fetchListAndLoadmore from '~/mixins/fetch-list-and-loadmore'
 
 export default {
   components: {
     UiMagazineFeatured,
     UiMagazineShowcaseItem,
+    UiInfiniteLoading,
   },
   middleware: ['authenticate'],
+  mixins: [
+    fetchListAndLoadmore({
+      maxResults() {
+        return 12
+      },
+
+      async fetchList(page) {
+        return await this.$fetchMagazines({
+          maxResults: 12,
+          page,
+          sort: '-publishedDate',
+          magazineType: 'special',
+        })
+      },
+
+      transformListItemContent(item) {
+        return this.mapDataToComponentProps?.(item) || {}
+      },
+    }),
+  ],
   async fetch() {
-    const response = await this.$fetchMagazines({
-      maxResults: 25,
-      sort: '-publishedDate',
-    })
-    this.setListData(response)
+    const [responseMagazineWeekly] = await Promise.allSettled([
+      this.$fetchMagazines({
+        maxResults: 8,
+        sort: '-publishedDate',
+        magazineType: 'weekly',
+      }),
+      this.initList(),
+    ])
+
+    if (responseMagazineWeekly.status === 'fulfilled') {
+      this.setListData(responseMagazineWeekly.value)
+    }
   },
   data() {
     return {
@@ -64,29 +126,33 @@ export default {
   },
   computed: {
     magazineFeatured() {
-      return this.listData[0]
+      return this.listData.slice(0, 2)
     },
-    magazineShowcases() {
-      return _.drop(this.listData)
+    magazineCurrent() {
+      return this.listData.slice(2, Infinity)
+    },
+    magazineSpecialListItemsInFirstPage() {
+      return this.listItems.slice(0, this.maxResults)
+    },
+    magazineSpecialListItemsInLoadmorePage() {
+      return this.listItems.slice(this.maxResults, Infinity)
+    },
+    magazineSpecial() {
+      return this.magazineSpecialListItemsInFirstPage.concat(
+        this.magazineSpecialListItemsInLoadmorePage
+      )
     },
   },
   methods: {
     mapDataToComponentProps(item) {
       return {
         id: item.id,
+        type: item.type,
         title: item.title,
         issue: item.issue,
         publishedDate: dayjs(item.publishedDate).format('YYYY/MM/DD'),
-        descriptions: getDescriptionsUnstyled(item),
         coverImgUrl: item.coverPhoto?.image?.resizedTargets?.mobile?.url,
         pdfLink: item?.magazine?.url,
-      }
-
-      function getDescriptionsUnstyled(item) {
-        const data = item?.description?.apiData ?? []
-        return data
-          .filter((paragraph) => paragraph.type === 'unstyled')
-          .map((paragraph) => paragraph?.content?.[0] ?? '')
       }
     },
     setListData(response = {}) {
@@ -111,22 +177,57 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+  @include media-breakpoint-up(xl) {
+    padding: 60px 0 0 0;
+  }
+
   &__current-featured-magazine-wrapper {
-    margin: 24px 0 0 0;
+    margin: 32px 0 0 0;
+    @include media-breakpoint-up(xl) {
+      margin: 48px 0 0 0;
+    }
   }
   &__magazine-showcase-list-wrapper {
     margin: 48px 0 0 0;
+    @include media-breakpoint-up(xl) {
+      margin: 109px 0 0 0;
+    }
+    & + & {
+      margin: 0;
+    }
   }
 }
 
 .current-featured-magazine-wrapper {
-  padding: 0 20px;
+  padding: 0 16px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  @include media-breakpoint-up(md) {
+    max-width: 528px;
+  }
+  @include media-breakpoint-up(xl) {
+    max-width: 1068px;
+    flex-direction: row;
+  }
+
+  &__magazine {
+    & + & {
+      margin: 24px 0 0 0;
+      @include media-breakpoint-up(xl) {
+        margin: 0 0 0 24px;
+      }
+    }
+  }
 }
 
 .title {
   font-size: 28px;
-  font-weight: 400;
+  font-weight: 500;
   color: #4a4a4a;
+  @include media-breakpoint-up(xl) {
+    font-size: 32px;
+  }
 }
 
 .magazine-showcase-list-wrapper {
@@ -138,13 +239,13 @@ export default {
   align-items: center;
   padding: 48px 20px;
   &__magazine-showcase-list {
-    margin: 24px 0 0 0;
+    margin: 32px 0 0 0;
     @include media-breakpoint-up(xl) {
-      margin: 40px 0 0 0;
+      margin: 48px 0 0 0;
     }
   }
   @include media-breakpoint-up(md) {
-    padding: 32px 48px;
+    padding: 48px;
   }
   @include media-breakpoint-up(xl) {
     padding: 48px 0;
