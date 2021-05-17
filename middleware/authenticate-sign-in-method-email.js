b8abb4d7870b8dcf193805bb988c7046b25af91a
kwhsiung <kaiwenhsiung@gmail.com>
2021-05-17 15:08:04 +0800

    feat(membership): bypass the sign in method check of auth in webview

--- middleware/authenticate-sign-in-method-email.js
+++ middleware/authenticate-sign-in-method-email.js
@@ -1,4 +1,9 @@
-export default function ({ req, store, redirect }) {
+import { shouldBypassAuthInAppWebview } from './utils'
+
+export default function ({ req, app, store, redirect }) {
+  if (shouldBypassAuthInAppWebview({ app })) {
+    return
+  }
   const signInProvider = store.state.membership.userSignInInfo.sign_in_provider
   if (signInProvider !== 'emailLink' && signInProvider !== 'password') {
     const from = req.url
