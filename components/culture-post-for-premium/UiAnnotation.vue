cfca7e727fcaa8a33088b7cd9df73f56d1f99f1b
kwhsiung <kaiwenhsiung@gmail.com>
2021-06-10 10:59:16 +0800

    feat(premium-page): redesign UiAnnotation

--- components/culture-post-for-premium/UiAnnotation.vue
+++ components/culture-post-for-premium/UiAnnotation.vue
@@ -2,21 +2,33 @@
   <span class="annotation">
     <span
       class="annotation__text"
-      :class="{ open: shouldOpenAnnotation }"
       @click="shouldOpenAnnotation = !shouldOpenAnnotation"
-      >{{ content.text }}</span
     >
+      {{ content.text }}
+      <SvgAnnotationToggle
+        :class="[
+          'toggle-icon',
+          { 'toggle-icon--rotate': shouldOpenAnnotation },
+        ]"
+      />
+    </span>
 
-    <span v-if="shouldOpenAnnotation" class="annotation__content">{{
-      content.pureAnnotationText
-    }}</span>
+    <span
+      v-if="shouldOpenAnnotation"
+      class="annotation__content"
+      v-text="content.pureAnnotationText"
+    />
   </span>
 </template>
 
 <script>
+import SvgAnnotationToggle from '~/assets/premium-annotation-toggle.svg?inline'
+
 export default {
   name: 'UiAnnotation',
-
+  components: {
+    SvgAnnotationToggle,
+  },
   props: {
     content: {
       type: Object,
@@ -24,7 +36,6 @@ export default {
       required: true,
     },
   },
-
   data() {
     return {
       shouldOpenAnnotation: false,
@@ -37,38 +48,33 @@ export default {
 .annotation {
   &__text {
     cursor: pointer;
-
-    &::after {
-      content: '';
-      display: inline-block;
-      margin-left: 5px;
-      vertical-align: middle;
-      border-style: solid;
-      border-width: 5px 5px 0 5px;
-      border-color: #3e67aa transparent transparent transparent;
-      transition: transform 0.3s ease-in;
-    }
-
-    &.open::after {
-      transform: rotate(-180deg);
-    }
+    display: inline-flex;
+    align-items: center;
   }
 
   &__content {
     display: inline-block;
     width: 100%;
-    margin-top: 20px;
-    margin-bottom: 20px;
+    margin-top: 8px;
+    margin-bottom: 32px;
     padding: 24px 32px;
     color: rgba(0, 0, 0, 0.87);
     font-size: 16px;
-    line-height: 29px;
     text-align: justify;
-    background-color: rgba(216, 216, 216, 0.3);
+    background-color: #f3f5f6;
     border-radius: 2px;
-    @include media-breakpoint-up(md) {
-      padding: 26px 37px;
-    }
+    line-height: 180%;
+  }
+}
+
+.toggle-icon {
+  display: inline-block;
+  height: 8px;
+  transform: rotate(180deg);
+  transition: transform 0.25s ease-out;
+  margin: 0 0 0 9px;
+  &--rotate {
+    transform: rotate(0deg);
   }
 }
 </style>
