e034507bca98d12f2f76c48af02a4b282a3d536a
kwhsiung <kaiwenhsiung@gmail.com>
2021-06-18 13:02:26 +0800

    feat(category-page): render pages/premiumcategory/_name.vue on /category/name if category is premium

---
+++ pages/premiumcategory/_name.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <UiBreadcrumb :breadcrumbs="breadcrumbs" />
+  </div>
+</template>
+
+<script>
+import UiBreadcrumb from '~/components/UiBreadcrumb.vue'
+import { usePremiumBreadcrumbs } from '~/composition/premium-breadcrumbs'
+
+export default {
+  layout: 'premium',
+  components: {
+    UiBreadcrumb,
+  },
+  setup() {
+    const breadcrumbs = usePremiumBreadcrumbs()
+    return {
+      breadcrumbs,
+    }
+  },
+}
+</script>
+
+<style scoped></style>
