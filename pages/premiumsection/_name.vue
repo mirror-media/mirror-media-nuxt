103e4ba7a8a1a68db69bace6fab734432e48b163
kwhsiung <kaiwenhsiung@gmail.com>
2021-06-18 12:57:14 +0800

    feat: add breadcrumbs feature to premiumsection page

--- pages/premiumsection/_name.vue
+++ pages/premiumsection/_name.vue
@@ -1,10 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <UiBreadcrumb :breadcrumbs="breadcrumbs" />
+  </div>
 </template>
 
 <script>
+import UiBreadcrumb from '~/components/UiBreadcrumb.vue'
+import { usePremiumBreadcrumbs } from '~/composition/premium-breadcrumbs'
+
 export default {
   layout: 'premium',
+  components: {
+    UiBreadcrumb,
+  },
+  setup() {
+    const breadcrumbs = usePremiumBreadcrumbs()
+    return {
+      breadcrumbs,
+    }
+  },
 }
 </script>
 
