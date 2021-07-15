16d42274a929f8c621f8a1923c758ca1e750df16
YiHsuan <yihsuanlee@mirrormedia.mg>
2021-07-15 16:15:11 +0800

    refactor(SubscribeFormOrdererData): template detal and check

--- components/SubscribeFormOrdererData.vue
+++ components/SubscribeFormOrdererData.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="orderer-data">
     <h2 class="subscribe-form__title">{{ type }}</h2>
-    <p v-if="!isOrderer" class="orderer-data__detail">
-      雜誌每週三出刊，週五前可收到雜誌。欲更改收件地址，請於上班時間來電告知。
-    </p>
-    <div v-if="!isOrderer" class="orderer-data__check">
-      <input
-        type="checkbox"
-        :value="disable"
-        @change="setDisable"
-        placeholder="我是範例"
-      /><span>同訂購人資訊</span>
-    </div>
-
+    <template v-if="!isOrderer">
+      <p class="orderer-data__detail">
+        雜誌每週三出刊，週五前可收到雜誌。欲更改收件地址，請於上班時間來電告知。
+      </p>
+      <div class="orderer-data__check">
+        <input
+          type="checkbox"
+          :value="disable"
+          @change="setDisable"
+          placeholder="我是範例"
+        /><span>同訂購人資訊</span>
+      </div>
+    </template>
     <div
       class="orderer-data__input_wrapper half"
       :class="{ error: $v.name.$error && isNeedToCheck }"
