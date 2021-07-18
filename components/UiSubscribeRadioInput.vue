d598d3aff8fcebc5aa2e5923d9796b3b6b9c5b89
YiHsuan <yihsuanlee@mirrormedia.mg>
2021-07-19 03:01:59 +0800

    feat(UiSubscribeRadioInput): add total description

--- components/UiSubscribeRadioInput.vue
+++ components/UiSubscribeRadioInput.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="radio-input">
-    <input
-      :checked="value === radioValue"
-      type="radio"
-      :value="radioValue"
-      @input="changeHandler"
-    />
-    <span class="radio">{{ radioName }}</span>
+    <div class="radio-input__input">
+      <input
+        :checked="value === radioValue"
+        type="radio"
+        :value="radioValue"
+        @input="changeHandler"
+      />
+      <span class="radio">{{ radioName }} </span>
+    </div>
+    <ul v-if="radioValue === '限時掛號'" class="radio-input__total">
+      <li>一年期加收 NT$ 1,040</li>
+      <li>二年期加收 NT$ 2,080</li>
+    </ul>
   </div>
 </template>
 
@@ -40,14 +46,23 @@ export default {
 
 <style lang="scss" scoped>
 .radio-input {
-  display: flex;
-  align-items: center;
   margin-bottom: 10px;
-  font-size: 18px;
-  line-height: normal;
+  &__input {
+    display: flex;
+    align-items: center;
+    font-size: 18px;
+    line-height: normal;
 
-  input[type='radio'] {
-    margin-right: 8px;
+    input[type='radio'] {
+      margin-right: 8px;
+    }
+  }
+
+  &__total {
+    margin-left: 50px;
+    font-size: 16px;
+    list-style: disc;
+    line-height: 1.5;
   }
 }
 </style>
