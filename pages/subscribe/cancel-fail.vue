056005feca4d08f163b1d8ee475784e52c545178
YiHsuan <yihsuanlee@mirrormedia.mg>
2021-08-03 19:46:37 +0800

    feat: create cancel-fail page

---
+++ pages/subscribe/cancel-fail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cancel-success">
+    <SubscribeCancel
+      :isAsk="false"
+      title="取消訂閱失敗"
+      description="請重新操作或聯繫客服由專人為您服務。</br>
+service@mirrormedia.mg</br>
+02-6633-3805"
+    />
+  </div>
+</template>
+
+<script>
+import SubscribeCancel from '~/components/SubscribeCancel.vue'
+
+export default {
+  components: { SubscribeCancel },
+}
+</script>
+
+<style lang="scss" scoped>
+.cancel-success {
+  min-height: calc(100vw - 150px);
+  padding: 40px 20px 0 20px;
+  @include media-breakpoint-up(sm) {
+    min-height: calc(100vw - 850px);
+    padding: 80px;
+  }
+}
+</style>
