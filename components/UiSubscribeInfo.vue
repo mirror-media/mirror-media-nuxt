424cdc1618ebd3af84d4b80033f6ccbadffe6809
YiHsuan <yihsuanlee@mirrormedia.mg>
2021-08-04 16:09:48 +0800

    feat: add SubscribeStepProgress and adjust UiSubscribeInfo

--- components/UiSubscribeInfo.vue
+++ components/UiSubscribeInfo.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="subscribe-info">
+  <div class="subscribe-info" :class="type">
     <h2>注意事項</h2>
     <ul>
       <li v-for="info in infoList" :key="info.id">
-        <p>{{ info.text }}</p>
+        <p v-html="info.text" />
         <p
           v-for="infoChild in infoSubList(info)"
           :key="infoChild.id"
@@ -18,49 +18,17 @@
 
 <script>
 export default {
-  data() {
-    return {
-      infoList: [
-        {
-          id: 0,
-          text: '完成訂購後，開始寄送期數將由服務人員提供。',
-        },
-        {
-          id: 1,
-          text:
-            '雜誌每週三出刊，將於週四(含)前完成寄送，若當週五仍未收到，請來電 02-6633-3882 查詢。',
-        },
-        {
-          id: 2,
-          text:
-            '若需更改收件地址，請於每週四中午 12:00 前來電，我們將於下週寄送至新址。',
-        },
-        {
-          id: 3,
-          text:
-            '訂單成立後，會收到我們 email 給您的確認信。手機號碼及電子信箱請務必正確填寫。',
-        },
-        {
-          id: 4,
-          text: '雜誌的取消/退訂注意事項',
-          child: [
-            {
-              id: 0,
-              text:
-                '雜誌未配送前可全額退款。雜誌如已開始配送，退費金額=原訂金額-(零售價 $75 x 訂閱已配送期數)-原訂金額之 5% 手續費',
-            },
-            {
-              id: 1,
-              text: '註1：訂閱已配送期數包含已進入配送作業流程的期數。',
-            },
-            {
-              id: 2,
-              text: '如有疑問請來電 02-6633-3882 詢問。',
-            },
-          ],
-        },
-      ],
-    }
+  props: {
+    type: {
+      type: String,
+      require: true,
+      default: 'papermag',
+    },
+    infoList: {
+      type: Array,
+      require: true,
+      default: () => [],
+    },
   },
   methods: {
     infoSubList(info) {
@@ -84,7 +52,7 @@ export default {
     line-height: normal;
     letter-spacing: normal;
     color: #000000de;
-    margin-bottom: 13px;
+    margin-bottom: 12px;
 
     @include media-breakpoint-up(sm) {
       font-size: 24px;
@@ -107,6 +75,14 @@ export default {
       .subscribe-info__sub {
         margin-left: 8px;
       }
+    }
+  }
+}
+.membership {
+  ul {
+    list-style: decimal;
+    li {
+      line-height: 150%;
     }
   }
 }
---
+++ pages/subscribe/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <SubscribeStepProgress :currentStep="1" />
+
+    <UiSubscribeInfo type="membership" :infoList="infoList" />
+  </div>
+</template>
+
+<script>
+import SubscribeStepProgress from '~/components/SubscribeStepProgress.vue'
+import UiSubscribeInfo from '~/components/UiSubscribeInfo.vue'
+
+export default {
+  components: {
+    SubscribeStepProgress,
+    UiSubscribeInfo,
+  },
+  data() {
+    return {
+      infoList: [
+        {
+          id: 0,
+          text: '月方案計算天數為 30 日，年方案計算天數為 365 日。',
+        },
+        {
+          id: 1,
+          text:
+            '月訂閱方案不適用消費者保護法第十九條第一項通訊交易七日解除權之規定，所以月費會員可取消繼續訂閱但無法申請當月退費。',
+        },
+        {
+          id: 2,
+          text:
+            '訂閱相關問題請 email 至會員專屬客服信箱 <a href = "mailto: MM-onlineservice@mirrormedia.mg" class="membership-subscribe__blue">MM-onlineservice@mirrormedia.mg</a>，我們會盡快為您協助處理。',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.membership-subscribe__blue {
+  color: #054f77;
+}
+</style>
