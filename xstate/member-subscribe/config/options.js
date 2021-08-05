019ddf42d6ee35a84a12bf298d3c5fcd60544d47
kwhsiung <kaiwenhsiung@gmail.com>
2021-08-05 18:24:13 +0800

    feat(xstate): init xstate logics of member subecribe

---
+++ xstate/member-subscribe/config/options.js
@@ -0,0 +1,44 @@
+import { assign } from 'xstate'
+
+export default {
+  guards: {
+    是否已登入: (context) => context.isLoggedIn,
+
+    是單篇付款過的會員: (context) => context.subscription === '解鎖這篇報導',
+    是月訂閱的會員: (context) => context.subscription === '月訂閱',
+    是年訂閱的會員: (context) => context.subscription === '年訂閱',
+
+    是否已確認訂購方案: (context) => context.subscriptionOrder,
+
+    是否從會員文章頁來的: (context) => context.isFromPost,
+
+    訂購: (context) => context.redirectDestination === 'purchaseForm',
+    方案: (context) => context.redirectDestination === 'sub',
+  },
+  actions: {
+    login: assign({
+      isLoggedIn: true,
+    }),
+    orderSubscribe: assign({
+      subscriptionOrder: (context, event) => {
+        return event.type
+      },
+    }),
+    subscribe: assign({
+      subscription: (context) => context.subscriptionOrder,
+      subscriptionOrder: null,
+    }),
+    cancelSubscribe: assign({
+      subscription: null,
+    }),
+    setFromPost: assign({
+      isFromPost: true,
+    }),
+    setRedirectToForm: assign({
+      redirectDestination: 'purchaseForm',
+    }),
+    setRedirectToSub: assign({
+      redirectDestination: 'sub',
+    }),
+  },
+}
