5ee5ace1881a79259771e1ef5ed87e8423f0b4e8
kwhsiung <kaiwenhsiung@gmail.com>
2021-08-05 18:21:12 +0800

    feat(xstate-inspector): toggle xstate inspector conditionally

--- plugins/xstate-inspector.client.js
+++ plugins/xstate-inspector.client.js
@@ -1,9 +1,14 @@
 import { inspect } from '@xstate/inspect'
+import { isMemberSubscribeFeatureToggled } from '~/xstate/member-subscribe/util'
 
-inspect({
-  /*
-   * options
-   * url: 'https://statecharts.io/inspect', // (default)
-   */
-  iframe: false, // open in new window
-})
+export default function (context) {
+  if (isMemberSubscribeFeatureToggled(context.route)) {
+    inspect({
+      /*
+       * options
+       * url: 'https://statecharts.io/inspect', // (default)
+       */
+      iframe: false, // open in new window
+    })
+  }
+}
