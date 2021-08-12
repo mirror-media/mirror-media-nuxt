982e0a213e1976f59aedad5af0c3f7216c8678d7
v61265 <66636137+v61265@users.noreply.github.com>
2021-08-12 16:32:08 +0800

    feat: subscribe purchase page UI (#379)
    * feat: add purchase-page
    
    * feat: add link
    
    Co-authored-by: YiHsuan <yihsuanlee@mirrormedia.mg>

---
+++ components/MembershipPayRecord.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="pay-record">
+    <div class="pay-record__title">
+      <div class="pay-record__title_status">付款紀錄</div>
+    </div>
+    <div class="pay-record__form">
+      <UiMembershipPayRecordList v-show="!isMobile" :pay="payListTitle" />
+      <UiMembershipPayRecordList
+        v-for="(pay, index) in payRecords"
+        :key="index"
+        :pay="pay"
+        :showMorePostButton="true"
+      />
+      <div
+        v-show="showMorePayRecordButton"
+        @click="$emit('load-more-record')"
+        class="pay-record__form_row load-more"
+      >
+        展開更多
+        <img src="~/assets/arrow-down-default.svg" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UiMembershipPayRecordList from '~/components/UiMembershipPayRecordList.vue'
+export default {
+  components: {
+    UiMembershipPayRecordList,
+  },
+  props: {
+    payRecords: {
+      type: Array,
+      require: true,
+      default: () => [],
+    },
+    showMorePayRecordButton: {
+      type: Boolean,
+      default: false,
+    },
+    isMobile: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      payListTitle: {
+        date: '購買日期',
+        number: '訂單編號',
+        method: '支付方式',
+        price: '金額',
+        type: '',
+        methodNote: '',
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.pay-record {
+  &__title {
+    &_status {
+      font-weight: 600;
+      font-size: 18px;
+      line-height: 25px;
+      color: rgba(0, 0, 0, 0.66);
+    }
+  }
+
+  &__form {
+    margin-top: 24px;
+    font-size: 15px;
+    line-height: 21px;
+    color: rgba(0, 0, 0, 0.87);
+    @include media-breakpoint-up(sm) {
+      font-size: 16px;
+      line-height: 150%;
+    }
+    &_row {
+      display: flex;
+      gap: 16px;
+      &:first-child {
+        padding-bottom: 8px;
+      }
+      &:not(:first-child) {
+        padding-top: 12px;
+        color: rgba(0, 0, 0, 0.5);
+        border-top: 1px solid rgba(0, 0, 0, 0.1);
+      }
+      &:nth-child(2) {
+        border-top: 1px solid rgba(0, 0, 0, 0.5) !important;
+      }
+      &:not(last-child) {
+        padding-bottom: 12px;
+      }
+      a {
+        color: #054f77;
+      }
+    }
+    .load-more {
+      color: #054f77;
+      justify-content: center;
+      cursor: pointer;
+      gap: 2px;
+      display: flex;
+      width: auto;
+    }
+  }
+}
+</style>
