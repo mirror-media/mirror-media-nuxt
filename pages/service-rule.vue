8cbc8e23acbf0939aed9dc706d5df9c0bc17b838
YiHsuan <yihsuanlee@mirrormedia.mg>
2021-08-17 18:34:47 +0800

    style: service-rule desktop style same as mobile

--- pages/service-rule.vue
+++ pages/service-rule.vue
@@ -4,23 +4,21 @@
     <div class="service-rule__description">
       歡迎加入鏡週刊會員！繼續使用前，請先詳閱鏡週刊服務條款。
     </div>
-    <div class="service-rule__content">
-      <div class="service-rule__content_wrapper">
-        <div v-for="paragraph in paragraphList" :key="paragraph.id">
-          <UiStoryContentHandler :paragraph="filterHTML(paragraph)" />
-        </div>
+    <div class="service-rule__wrapper">
+      <div v-for="paragraph in paragraphList" :key="paragraph.id">
+        <UiStoryContentHandler :paragraph="filterHTML(paragraph)" />
       </div>
-      <div class="service-rule__content_agree">
-        <label>
-          <input type="checkbox" v-model="isChecked" :checked="isChecked" />
-          <span>我同意以上條款</span>
-        </label>
-        <UiMembershipButtonPrimary
-          :disabled="!isChecked"
-          @click.native="handleSubmit"
-          >繼續登入</UiMembershipButtonPrimary
-        >
-      </div>
+    </div>
+    <div class="service-rule__agree">
+      <label>
+        <input type="checkbox" v-model="isChecked" :checked="isChecked" />
+        <span>我同意以上條款</span>
+      </label>
+      <UiMembershipButtonPrimary
+        :disabled="!isChecked"
+        @click.native="handleSubmit"
+        >繼續登入</UiMembershipButtonPrimary
+      >
     </div>
   </div>
 </template>
@@ -110,10 +108,6 @@ export default {
   margin: 24px auto;
   padding: 20px;
 
-  @include media-breakpoint-up(md) {
-    padding: 0px;
-  }
-
   &__title {
     font-size: 20px;
     line-height: 32px;
@@ -127,74 +121,62 @@ export default {
     margin-top: 4px;
   }
 
-  &__content {
+  &__wrapper {
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    padding: 21px 12px;
+    height: calc(100vh - 500px);
+    overflow-y: scroll;
     margin-top: 12px;
     font-size: 13px;
     line-height: 18px;
     color: rgba(0, 0, 0, 0.87);
     @include media-breakpoint-up(md) {
-      border: 1px solid rgba(0, 0, 0, 0.1);
-      border-radius: 4px;
-      padding: 40px;
-      max-height: calc(100% - 233px);
+      padding: 40px 35px 40px 40px;
     }
 
-    &_wrapper {
-      border: 1px solid rgba(0, 0, 0, 0.1);
-      padding: 21px 12px;
-      height: calc(100vh - 500px);
-      overflow: scroll;
-      @include media-breakpoint-up(md) {
-        border: 0;
-        padding: 0;
-        height: auto;
-        overflow: inherit;
-      }
+    * + * {
+      margin-top: 1.5rem;
+    }
 
-      * + * {
-        margin-top: 1.5rem;
-      }
+    h2 {
+      margin-top: 0px;
+    }
 
-      h2 {
-        margin-top: 0px;
-      }
-
-      ul {
-        li {
-          list-style-type: disc;
-          margin-left: 20px;
-          &::marker {
-            font-size: 15px;
-          }
+    ul {
+      li {
+        list-style-type: disc;
+        margin-left: 20px;
+        &::marker {
+          font-size: 15px;
         }
       }
     }
+  }
 
-    &_agree {
-      font-size: 18px;
-      line-height: 25px;
-      color: rgba(0, 0, 0, 0.87);
-      margin-top: 32px;
+  &__agree {
+    font-size: 18px;
+    line-height: 25px;
+    color: rgba(0, 0, 0, 0.87);
+    margin-top: 32px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    label {
       display: flex;
       align-items: center;
-      flex-direction: column;
 
-      label {
-        display: flex;
-        align-items: center;
-
-        input[type='checkbox'] {
-          width: 16px;
-          height: 16px;
-          margin-right: 8px;
-        }
+      input[type='checkbox'] {
+        width: 16px;
+        height: 16px;
+        margin-right: 8px;
       }
     }
+  }
 
-    .button {
-      width: 240px;
-      margin-top: 24px;
-    }
+  .button {
+    width: 240px;
+    margin-top: 24px;
   }
 }
 </style>
