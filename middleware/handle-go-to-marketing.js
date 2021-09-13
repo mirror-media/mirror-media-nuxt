f0a6d888254ebfd9c8ab51665abd4a2afcad30cd
kwhsiung <kaiwenhsiung@gmail.com>
2021-09-13 18:57:42 +0800

    fix(marketing-middleware): fix missing basicInfo

--- middleware/handle-go-to-marketing.js
+++ middleware/handle-go-to-marketing.js
@@ -1,6 +1,6 @@
 export default function ({ store, redirect }) {
   const isMarketingMember =
-    store.state['membership-subscribe'].type === 'marketing'
+    store.state['membership-subscribe'].basicInfo.type === 'marketing'
   if (isMarketingMember) {
     redirect('/marketing')
   }
