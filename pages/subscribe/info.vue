e43d2396610e0f70799aa851fefc4dc33d6697cd
LIYI <liyibass@gmail.com>
2021-09-14 20:10:02 +0800

    feat(subscribe): add redirect-to-newwebpay page's basic structure(with fake payload)

--- pages/subscribe/info.vue
+++ pages/subscribe/info.vue
@@ -65,6 +65,7 @@
 </template>
 
 <script>
+import qs from 'qs'
 import { required, email } from 'vuelidate/lib/validators'
 import { ENV } from '~/configs/config'
 import SubscribeStepProgress from '~/components/SubscribeStepProgress.vue'
@@ -186,10 +187,21 @@ export default {
       console.log(result)
 
       // TODO: fire form post
+      const paymentPayload = {
+        MerchantID: 'MS315799494',
+        TradeInfo:
+          '2d33a862e529fbf07b264c6abfc079ab5aad16897d81a77202029432eb21c3b750491bcdfea54d0609db24b6553a640c6734567fb8d3171f57b928b4dc83d3f6177683a8d25c2058097133d8daa5ff93c9a1a0d432b61ad1628f297d82d300e57296c9ed8286c8409fac8f1fbac7881e1c317776c4549de1d62ae1549c34b2e6c4aeaac031a2d866a20a34c091e726ab78375b4f0d91158d283efd79b5d7a7561c4005dcda53609abbcaeabbd82beede45dfddcdf1b4b34b7e27d6d1b702409d97c2bb21bd3a342e3cc5d7efb42d1a85145f409c08d18f6290f89aee99896d13319231721b6ce70d5747506ee2195f492d60c4adb83cd6bab3aa30a97613cef3aa86b51e5ffe54f487c31f0bbf99ea1678472b70ac902e6b21b74685c429b88ade11b94196c4496862278227129390abcdff7225389eaf0a2a8ad6b982090dab2a783c69aa9dfb546fac7563e6dd2df3899427c43ae1d1961f101699b46f2861a14042c93b3ac363cce47dd7a07bd157ddbddcd369bd080d78436bdb3f0bfdfdf2a67b2d1e9cf8337d0d0813437b9bf6f6611aba7571dd7520c5c9d2706729c6a665a3deae9464293c7d2301e8acb642f2c6e3ed0abe8496e66b2604dd3d1cc78b7b4a0c78297d8d91b02836b5f98b7abe44c7b01d108b4290c4b285b46819b985ef9d4845b087b326aa2e3fed9524e351cb5fc3aac6ef74e4785f05e0dc7541b80ff23d3801929100bd03d8107b910a053e67c48d4e3cf01ed0fc5d5a4ecf46a19a58a1b4c4cfc59fa3349ff9cd127856825e633c49093cb1b286d9e9b70532c8119b6357ac8abb06fc8ecd8bb2dd9b4de216dea02ba1da6b49521f37d0e4502140795985c69ef1e99a2db13aa1c50803aaa686b1af1ebb1206068cb7caef02bc7cad0ac050a51a9e4edb7ad8299486e144d4615fa8f1d5dc33f88256fa475feea4e180a433d48f2f95ed07c1658c3155c427ad30ba773c746900242320091c',
+        TradeSha:
+          'E202663F3A136E01C2B640BC117DC8C91450A47E475998CB65BA54729A36A353',
+        Version: 1.6,
+      }
+      const queryString = qs.stringify(paymentPayload)
 
-      if (this.orderStatus === 'success')
-        return this.sendMembershipSubscribe('付款成功')
-      this.sendMembershipSubscribe('付款失敗')
+      this.$router.push(`/subscribe/redirect?${queryString}`)
+
+      // if (this.orderStatus === 'success')
+      //   return this.sendMembershipSubscribe('付款成功')
+      // this.sendMembershipSubscribe('付款失敗')
     },
     async getPaymentDataFromApiGateWay() {
       let gateWayPayload
---
+++ pages/subscribe/redirect.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="redirect-page">
+    <NewebpayForm
+      :merchantId="paymentPayload.MerchantID"
+      :tradeInfo="paymentPayload.TradeInfo"
+      :tradeSHA="paymentPayload.TradeSHA"
+      :version="paymentPayload.Version"
+    />
+  </div>
+</template>
+
+<script>
+import NewebpayForm from '~/components/NewebpayForm.vue'
+export default {
+  components: {
+    NewebpayForm,
+  },
+  layout: 'empty',
+  middleware({ store, redirect }) {
+    // block access direct from url
+    const isReadyToPay = store.getters['subscribe/isReadyToPay']
+    if (!isReadyToPay) {
+      // redirect('/papermag')
+    }
+  },
+  computed: {
+    paymentPayload() {
+      const queryString = this.$route.query
+      return {
+        MerchantID: queryString.MerchantID,
+        TradeInfo: queryString.TradeInfo,
+        TradeSha: queryString.TradeSha,
+        Version: parseInt(queryString.Version),
+      }
+    },
+  },
+  mounted() {
+    // submit newebpay form-post to redirect to newebpay page
+    const formDOM = document.forms.newebpay
+    formDOM.submit()
+  },
+  methods: {
+    storeToSessionStorage(name, value) {
+      if (process.browser) {
+        sessionStorage.setItem(name, value)
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
