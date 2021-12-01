a7f2995914fd1c5fa567d1908600cdde7bc5526f
YiHsuan <yihsuanlee@mirrormedia.mg>
2021-12-01 17:05:32 +0800

    feat: add experience of dev, staging and prod

--- experiments/normal-post-popular-redesign/index.js
+++ experiments/normal-post-popular-redesign/index.js
@@ -7,19 +7,19 @@ export default {
       ENV: 'local',
       id: 'XcYLS9eIS3uUk35UvIK1lA',
     },
-    // {
-    //   ENV: 'dev',
-    //   id: 'ZP88t_4BQg6qYZ2wUfgC4w',
-    // },
+    {
+      ENV: 'dev',
+      id: '6p2Sz7YuSO-xu1Yg7AwUhw',
+    },
 
-    // // Google Optimize Container OPT-N9L3WX3
-    // {
-    //   ENV: 'staging',
-    //   id: 'jKvOBLdKTw2fJaDIbw3rLw',
-    // },
-    // {
-    //   ENV: 'prod',
-    //   id: '7vhXna74R_G8vihAeKZDTA',
-    // },
+    // Google Optimize Container OPT-N9L3WX3
+    {
+      ENV: 'staging',
+      id: 'G65uUSpwR-6TVYg8yWXIqQ',
+    },
+    {
+      ENV: 'prod',
+      id: '9tTTBN9bS26v3-mZ3bfjyg',
+    },
   ],
 }
