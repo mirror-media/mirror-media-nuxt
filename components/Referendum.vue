3db9182bbeee25690aaf05dd373379e9770f6d02
YiHsuan <yihsuanlee@mirrormedia.mg>
2021-12-09 14:08:52 +0800

    feat: add referendum iframe on dev hompage

---
+++ components/Referendum.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="isDev" class="referendum">
+    <div class="referendum__wrapper">
+      <iframe src="https://dev.readr.tw/referendum" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ENV } from '~/configs/config'
+export default {
+  computed: {
+    isDev() {
+      return ENV === 'local' || ENV === 'dev'
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.referendum {
+  height: 466px;
+  width: 100%;
+  @include media-breakpoint-up(md) {
+    height: 265px;
+  }
+  &__wrapper {
+    position: absolute;
+    left: 0;
+    width: 100vw;
+    padding: 10px;
+    iframe {
+      height: 446px;
+      width: 100%;
+      @include media-breakpoint-up(md) {
+        height: 245px;
+      }
+    }
+  }
+}
+</style>
