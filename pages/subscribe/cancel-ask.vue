3775d2f5b8cb77d49997540d3766bbfce0b0f02d
KJ WEN <kjwen310@gmail.com>
2021-12-23 11:59:49 +0800

    feat: adjust the identify-methods of app-subscriber and apply to subscribe/cancel-ask pages (#464)
    * feat: adjust the identify-methods of app-subscriber and apply to subscribe/cancel-ask pages
    
    * feat: add loading cover when data fetching
    
    * refactor: remove unused console and refactor generatePayMethodText function

--- pages/subscribe/cancel-ask.vue
+++ pages/subscribe/cancel-ask.vue
@@ -1,56 +1,58 @@
 <template>
   <div class="cancel-ask">
-    <SubscribeWrapper v-if="isPayByApp">
-      <h6 class="cancel-ask__title">取消訂閱提示</h6>
-      <p class="cancel-ask__description">
-        由於您先前於 APP 購買，如要取消訂閱，請至 App Store (iOS 系統) 或 Google
-        Play (Android 系統) 操作
-      </p>
-      <UiMembershipButtonPrimary
-        class="subscribe-cancel__back"
-        @click.native="handleBack"
-        >回訂閱紀錄</UiMembershipButtonPrimary
-      >
-    </SubscribeWrapper>
-    <SubscribeWrapper v-else @back="handleBack" @submit="handleSubmit">
-      <h6 class="cancel-ask__title">取消訂閱</h6>
-      <p class="cancel-ask__description">
-        請問您為何想取消訂閱鏡週刊 Premium 服務？
-      </p>
-      <UiMembershipCheckoutLabel
-        content="文章無法滿足需求"
-        @change="handleChange"
-      />
-      <UiMembershipCheckoutLabel
-        content="已訂閱其他媒體"
-        @change="handleChange"
-      />
-      <UiMembershipCheckoutLabel
-        content="使用體驗不佳"
-        @change="handleChange"
-      />
-      <UiMembershipCheckoutLabel
-        content="想改用單篇付費方式繼續閱讀"
-        @change="handleChange"
-      />
-      <UiMembershipCheckoutLabel content="其他" @change="handleChange" />
-      <textarea
-        v-if="shouldShowTextarea"
-        v-model="otherDetail"
-        placeholder="請輸入您的回饋..."
-        rows="3"
-      />
-      <div class="cancel-ask__button_group">
-        <UiMembershipButtonSecondary @click.native="handleBack"
-          >返回</UiMembershipButtonSecondary
+    <template v-if="doesHaveIsPayByAppValue">
+      <SubscribeWrapper v-if="isPayByApp">
+        <h6 class="cancel-ask__title">取消訂閱提示</h6>
+        <p class="cancel-ask__description">
+          由於您先前於 APP 購買，如要取消訂閱，請至 App Store (iOS 系統) 或
+          Google Play (Android 系統) 操作
+        </p>
+        <UiMembershipButtonPrimary
+          class="subscribe-cancel__back"
+          @click.native="handleBack"
+          >回訂閱紀錄</UiMembershipButtonPrimary
         >
-        <UiMembershipButtonPrimary @click.native="handleSubmit"
-          >確認取消訂閱</UiMembershipButtonPrimary
-        >
-      </div>
-    </SubscribeWrapper>
-
-    <UiLoadingCover v-if="isLoading" />
+      </SubscribeWrapper>
+      <SubscribeWrapper v-else @back="handleBack" @submit="handleSubmit">
+        <h6 class="cancel-ask__title">取消訂閱</h6>
+        <p class="cancel-ask__description">
+          請問您為何想取消訂閱鏡週刊 Premium 服務？
+        </p>
+        <UiMembershipCheckoutLabel
+          content="文章無法滿足需求"
+          @change="handleChange"
+        />
+        <UiMembershipCheckoutLabel
+          content="已訂閱其他媒體"
+          @change="handleChange"
+        />
+        <UiMembershipCheckoutLabel
+          content="使用體驗不佳"
+          @change="handleChange"
+        />
+        <UiMembershipCheckoutLabel
+          content="想改用單篇付費方式繼續閱讀"
+          @change="handleChange"
+        />
+        <UiMembershipCheckoutLabel content="其他" @change="handleChange" />
+        <textarea
+          v-if="shouldShowTextarea"
+          v-model="otherDetail"
+          placeholder="請輸入您的回饋..."
+          rows="3"
+        />
+        <div class="cancel-ask__button_group">
+          <UiMembershipButtonSecondary @click.native="handleBack"
+            >返回</UiMembershipButtonSecondary
+          >
+          <UiMembershipButtonPrimary @click.native="handleSubmit"
+            >確認取消訂閱</UiMembershipButtonPrimary
+          >
+        </div>
+      </SubscribeWrapper>
+    </template>
+    <!-- if fetch is not complete, or form is not submitted, show loading-->
+    <UiLoadingCover v-if="$fetchState.pending || isLoading" />
   </div>
 </template>
 
@@ -78,10 +80,15 @@ export default {
       sendMembershipSubscribe: send,
     }
   },
+  async fetch() {
+    // check if user's subscription is paid by mobile
+    const isMemberPaidWithMobile = await this.$isMemberPaidSubscriptionWithMobile()
+    this.isPayByApp = isMemberPaidWithMobile
+  },
   data() {
     return {
       isLoading: false,
-      isPayByApp: false,
+      isPayByApp: undefined,
       reason: [],
       cancelStatus: 'success',
       otherDetail: '',
@@ -96,14 +103,9 @@ export default {
         ? `取消原因：${this.reason.join('、')}。`
         : '取消原因：未填寫。'
     },
-  },
-  async created() {
-    // check if user's newest subscription is paid by mobile
-    const isMemberPaidWithMobile = await this.$isMemberPaidSubscriptionWithMobile()
-
-    return {
-      isPayByApp: isMemberPaidWithMobile,
-    }
+    doesHaveIsPayByAppValue() {
+      return this.isPayByApp !== undefined
+    },
   },
   methods: {
     handleBack() {
@@ -125,9 +127,6 @@ export default {
         this.sendMembershipSubscribe('確認取消訂閱失敗')
       }
     },
-    setIsPayByApp(val) {
-      this.isPayByApp = val
-    },
     setCancelStatus(val) {
       this.cancelStatus = val
     },
@@ -143,7 +142,7 @@ export default {
 
 <style lang="scss" scoped>
 .cancel-ask {
-  min-height: calc(100vw - 150px);
+  min-height: calc(100vh - 150px);
   padding: 40px 20px 0 20px;
   @include media-breakpoint-up(sm) {
     min-height: calc(100vw - 850px);
