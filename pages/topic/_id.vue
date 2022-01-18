7863f7675bce2291c0ffae053eeea9184cc9e922
kwhsiung <kaiwenhsiung@gmail.com>
2022-01-18 13:15:27 +0800

    chore(topic-page): add comment about deprecated topic page

--- pages/topic/_id.vue
+++ pages/topic/_id.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- CAVEAT: we replace the /topic/:id route component with deprecated one because of refactoring legacy topic page in progress -->
+  <!-- CAVEAT: we replace the /topic/:id route component with deprecated one in deprecated/topic-page/Topic.vue because of refactoring legacy topic page in progress -->
   <!-- so page component you see here was disabled temporarily -->
   <!-- see extendRoutes settings in nuxt.config.js -->
   <section class="topic"></section>
