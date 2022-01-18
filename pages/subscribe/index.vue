2073e7d608aef68fbcdc82a08a1ee5b571f550e3
kwhsiung <kaiwenhsiung@gmail.com>
2022-01-18 11:18:40 +0800

    feat(subscribe-page): move template of pay by app out of feature toggle

--- pages/subscribe/index.vue
+++ pages/subscribe/index.vue
@@ -3,7 +3,23 @@
     <template v-if="!canShowFeat && doesHaveIsPayByAppValue">
       <SubscribeStepProgress :currentStep="1" />
       <ClientOnly>
-        <template v-if="memberStatus !== 'year'">
+        <template v-if="isPayByApp">
+          <div class="subscribe-choose__textcard">
+            <SubscribeWrapper>
+              <h6 class="subscribe-choose__textcard_title">想要變更方案嗎？</h6>
+              <div class="subscribe-choose__textcard_description">
+                由於您先前於 APP 購買，如要變更方案，請至 App Store (iOS 系統)
+                或 Google Play (Android 系統) 操作。
+              </div>
+              <UiMembershipButtonPrimary
+                class="subscribe-choose__textcard_back"
+                @click.native="handleGoToSectionMember"
+                >回會員專區</UiMembershipButtonPrimary
+              >
+            </SubscribeWrapper>
+          </div>
+        </template>
+        <template v-else-if="memberStatus !== 'year'">
           <div class="subscribe-choose__wrapper">
             <h2
               v-if="memberStatus !== 'month'"
@@ -46,71 +62,6 @@
       </ClientOnly>
     </template>
 
-    <template v-if="canShowFeat && doesHaveIsPayByAppValue">
-      <SubscribeStepProgress :currentStep="1" />
-      <ClientOnly>
-        <template v-if="isPayByApp">
-          <div class="subscribe-choose__textcard">
-            <SubscribeWrapper>
-              <h6 class="subscribe-choose__textcard_title">想要變更方案嗎？</h6>
-              <div class="subscribe-choose__textcard_description">
-                由於您先前於 APP 購買，如要變更方案，請至 App Store (iOS 系統)
-                或 Google Play (Android 系統) 操作。
-              </div>
-              <UiMembershipButtonPrimary
-                class="subscribe-choose__textcard_back"
-                @click.native="handleGoToSectionMember"
-                >回會員專區</UiMembershipButtonPrimary
-              >
-            </SubscribeWrapper>
-          </div>
-        </template>
-        <template v-else>
-          <template v-if="memberStatus !== 'year'">
-            <div class="subscribe-choose__wrapper">
-              <h2
-                v-if="memberStatus !== 'month'"
-                class="subscribe-choose__wrapper_title"
-                :class="{ basic: memberStatus === 'basic' }"
-              >
-                方案選擇
-              </h2>
-              <div class="subscribe-choose__wrapper_plans">
-                <SubscribeMembershipChoosePlanCard
-                  v-for="plan in planShowed"
-                  :key="plan.title"
-                  :title="plan.title"
-                  :details="plan.details"
-                  :buttons="plan.buttons"
-                  :hintUnderButton="hintUnderButton"
-                  @subscribePlan="handleSubscribePlan"
-                  @login="sendMembershipSubscribe('點擊免費加入會員')"
-                />
-              </div>
-            </div>
-            <UiSubscribeInfo type="membership" :infoList="infoList" />
-          </template>
-          <template v-else>
-            <div class="subscribe-choose__textcard">
-              <SubscribeWrapper>
-                <h6 class="subscribe-choose__textcard_title">
-                  想要變更方案嗎？
-                </h6>
-                <div class="subscribe-choose__textcard_description">
-                  您目前訂閱的方案為<span>鏡週刊 Premium 服務-年訂閱方案</span
-                  >。如需變更，請先取消目前的方案，再重新訂閱新的方案。
-                </div>
-                <UiMembershipButtonPrimary
-                  class="subscribe-choose__textcard_back"
-                  @click.native="handleSet"
-                  >前往付款設定</UiMembershipButtonPrimary
-                >
-              </SubscribeWrapper>
-            </div>
-          </template>
-        </template>
-      </ClientOnly>
-    </template>
     <!-- if fetch is not complete, show loading-->
     <UiLoadingCover v-if="canShowFeat && $fetchState.pending" />
   </div>
