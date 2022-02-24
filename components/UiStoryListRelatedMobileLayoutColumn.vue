cae92924ef8d7eae99350ddda470d673a511002a
kwhsiung <kaiwenhsiung@gmail.com>
2022-02-24 16:48:24 +0800

    feat(UiStoryListRelatedMobileLayoutColumn): add stylings of tablet and desktop

--- components/UiStoryListRelatedMobileLayoutColumn.vue
+++ components/UiStoryListRelatedMobileLayoutColumn.vue
@@ -65,6 +65,10 @@ export default {
 .story-list-related {
   padding: 48px 20px;
   background-color: #eeeeee;
+  @include media-breakpoint-up(md) {
+    background-color: transparent;
+  }
+
   &__item {
     & + & {
       margin: 24px 0 0 0;
@@ -75,11 +79,50 @@ export default {
 .item {
   display: flex;
   flex-direction: column-reverse;
+  @include media-breakpoint-up(md) {
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: #eeeeee;
+    border-left: 7.72px solid #808080;
+  }
+
   &__title {
     margin: 12px 0 0 0;
     font-size: 18px;
     line-height: 1.5;
     color: rgba(0, 0, 0, 0.87);
+    @include media-breakpoint-up(md) {
+      margin: 0 0 0 18px;
+      display: flex;
+      align-items: center;
+      line-height: 1.3;
+      color: #808080;
+      a {
+        word-wrap: break-word;
+        -webkit-line-clamp: 2;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+      }
+    }
+  }
+
+  &__image {
+    @include media-breakpoint-up(md) {
+      min-width: 86.91px;
+      width: 86.91px;
+      min-height: 90px;
+      height: 90px;
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+    @include media-breakpoint-up(xl) {
+      min-width: 135px;
+      width: 135px;
+    }
   }
 }
 </style>
