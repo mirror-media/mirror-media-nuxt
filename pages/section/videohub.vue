abbe777bb07909febc68f26d0c8dbd04c25db571
YiHsuan <yihsuanlee@mirrormedia.mg>
2022-03-16 02:42:52 +0800

    style: premium no ad page of video page

--- pages/section/videohub.vue
+++ pages/section/videohub.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="section">
+  <section class="section" :class="{ premium: isPremiumMember }">
     <UiVideoIframeWithItems
       :items="latestData"
       textPositionInMdViewport="right"
@@ -10,15 +10,27 @@
         <h1>最新影片</h1>
       </template>
     </UiVideoIframeWithItems>
-    <div class="section__bottom-wrapper" :class="{ grey: isPremium }">
+    <div class="section__bottom-wrapper" :class="{ grey: isPremiumMember }">
       <UiVideoPopular
+        v-if="!isPremiumMember"
         :items="popularData"
         class="section__popular"
         @sendGa="handleSendGa"
       />
       <UiVideoSubscriptions class="section__subscriptions" />
+      <UiVideoPopular
+        v-if="isPremiumMember"
+        :items="popularData"
+        class="section__popular"
+        @sendGa="handleSendGa"
+      />
 
-      <ContainerGptAd class="section__ad" pageKey="videohub" adKey="FT" />
+      <ContainerGptAd
+        v-if="!isPremiumMember"
+        class="section__ad"
+        pageKey="videohub"
+        adKey="FT"
+      />
 
       <div class="section__categories-wrapper">
         <UiVideoCategory
@@ -55,7 +67,6 @@ import UiVideoIframeWithItems from '~/components/UiVideoIframeWithItems.vue'
 import UiVideoPopular from '~/components/UiVideoPopular.vue'
 import UiVideoSubscriptions from '~/components/UiVideoSubscriptions.vue'
 import UiYoutubePolicies from '~/components/UiYoutubePolicies.vue'
-import { ENV } from '~/configs/config'
 
 const INVERTED_PLAYLIST_MAPPING = _.invert(PLAYLIST_MAPPING)
 
@@ -96,13 +107,8 @@ export default {
         .map(this.mapCategoryToPlatlistId)
         .filter((playlist) => playlist)
     },
-    isPremium() {
-      if (ENV === 'staging' || ENV === 'prod') return false
-      const memberType = this.$store?.state['membership-subscribe']?.basicInfo
-        .type
-      return (
-        memberType === 'subscribe_monthly' || memberType === 'subscribe_yearly'
-      )
+    isPremiumMember() {
+      return this.$store?.getters?.['membership-subscribe/isPremiumMember']
     },
   },
   beforeMount() {
@@ -194,6 +200,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$categories: (
+  'coverstory': #30bac8,
+  'entertainment': #bf3385,
+  'society': #8b572a,
+  'investigation': #417505,
+  'people': #4a90e2,
+  'finance': #8b222c,
+  'foodtravel': #f1a356,
+  'ent_perspective': #30bac8,
+  'carandwatch': #969696,
+);
 .section {
   overflow: hidden;
   &__latest {
@@ -274,6 +291,315 @@ export default {
     @include media-breakpoint-up(xl) {
       order: 3;
       margin-bottom: 20px;
+    }
+  }
+
+  &.premium::v-deep {
+    margin: 0 auto;
+    h1 {
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 115%;
+      @include media-breakpoint-up(md) {
+        font-size: 20.8px;
+        line-height: 115%;
+        width: 640px;
+      }
+      @include media-breakpoint-up(xl) {
+        margin: 40px 0 0 0;
+        width: 1024px;
+      }
+    }
+    .video-iframe-items {
+      &__remaining {
+        margin-top: 16px;
+        max-width: 1024px;
+        @include media-breakpoint-up(md) {
+          width: 80%;
+          max-width: 640px;
+        }
+        @include media-breakpoint-up(xl) {
+          margin: 20px 0 0 36px;
+        }
+
+        .linked-item {
+          & + .linked-item {
+            margin-top: 16px;
+            @include media-breakpoint-up(xl) {
+              margin-top: 10px;
+            }
+          }
+          &__image {
+            @include media-breakpoint-up(md) {
+              width: 258px;
+              height: 145px;
+            }
+            @include media-breakpoint-up(xl) {
+              width: 135px;
+              padding-top: 76px;
+              height: 0;
+            }
+          }
+
+          &__title {
+            font-weight: 400;
+            font-size: 14px;
+            line-height: 20px;
+            margin-top: 8px;
+            @include media-breakpoint-up(md) {
+              font-size: 24px;
+              line-height: 34px;
+              margin-top: 0;
+              margin-left: 25px;
+            }
+            @include media-breakpoint-up(xl) {
+              font-size: 18px;
+              line-height: 25px;
+              margin-left: 16px;
+            }
+          }
+        }
+      }
+      &__first {
+        @include media-breakpoint-up(xl) {
+          width: 480px;
+          &-title {
+            font-weight: 400;
+            line-height: 32px;
+            margin-top: 8px;
+          }
+        }
+      }
+    }
+
+    .video-subscriptions {
+      box-shadow: none;
+      margin: 0 20px;
+    }
+
+    .video-popular {
+      padding: 18px 0 20px 0px;
+      border-top: 0;
+      position: relative;
+      @include media-breakpoint-up(xl) {
+        width: 272px;
+        padding: 0 0 28px 0;
+      }
+
+      &:before {
+        content: '';
+        width: calc(100vw);
+        height: 1px;
+        background: #979797;
+        position: absolute;
+        display: block;
+        top: 0;
+        right: 0;
+        @include media-breakpoint-up(md) {
+          width: calc(100vw - 40px);
+          right: 20px;
+        }
+        @include media-breakpoint-up(xl) {
+          content: none;
+        }
+      }
+      &:after {
+        content: '';
+        width: calc(100vw);
+        height: 1px;
+        background: #979797;
+        position: absolute;
+        display: block;
+        bottom: 0;
+        right: 0;
+        @include media-breakpoint-up(md) {
+          width: calc(100vw - 40px);
+          right: 20px;
+        }
+        @include media-breakpoint-up(xl) {
+          content: none;
+        }
+      }
+
+      &__heading {
+        font-weight: 600;
+        font-size: 16px;
+        line-height: 115%;
+        letter-spacing: 0.5px;
+        @include media-breakpoint-up(md) {
+          margin: 0;
+        }
+        @include media-breakpoint-up(xl) {
+          font-weight: 600;
+          font-size: 20.8px;
+          line-height: 115%;
+        }
+      }
+      .scroll-container {
+        margin-top: 16px;
+        .linked-item {
+          @include media-breakpoint-up(xl) {
+            width: 244px;
+          }
+          &:first-child {
+            margin-left: 16px;
+            @include media-breakpoint-up(md) {
+              margin-left: 20px;
+            }
+            @include media-breakpoint-up(xl) {
+              margin-left: 0;
+              margin-top: 20px;
+            }
+          }
+          &:not(:first-child) {
+            margin-left: 24px;
+            @include media-breakpoint-up(xl) {
+              margin-left: 0;
+            }
+          }
+          &__image {
+            width: 169px;
+            @include media-breakpoint-up(xl) {
+              width: 100%;
+            }
+          }
+          &__title {
+            @include media-breakpoint-up(xl) {
+              margin-top: 16px;
+              font-weight: 400;
+              font-size: 16px;
+              line-height: 150%;
+            }
+          }
+        }
+      }
+    }
+
+    .section__categories-wrapper {
+      @include media-breakpoint-up(xl) {
+        margin-left: 296px;
+        padding: 0 0 0 24px;
+      }
+    }
+
+    .video-category {
+      padding: 23px 16px 77px 16px;
+      @include media-breakpoint-up(md) {
+        padding: 28px 0;
+        margin: 0 20px;
+      }
+      @include media-breakpoint-up(xl) {
+        padding: 0 0 28px 0;
+        margin: 0;
+        &:not(:first-child) {
+          padding-top: 28px;
+          border-top: 1px solid #c4c4c4;
+        }
+      }
+
+      &__remaining {
+        @include media-breakpoint-up(md) {
+          margin: 16px 0 0 20px;
+        }
+      }
+
+      &__heading {
+        font-weight: 600;
+        font-size: 16px;
+        line-height: 115%;
+        letter-spacing: 0.5px;
+        @include media-breakpoint-up(md) {
+          font-size: 20.8px;
+          line-height: 115%;
+          margin: 0;
+        }
+        &::before {
+          content: none;
+        }
+        & + * {
+          margin-top: 16px;
+        }
+      }
+
+      .linked-item {
+        & + .linked-item {
+          margin-top: 16px;
+          @include media-breakpoint-up(md) {
+            margin-top: 10px;
+          }
+        }
+
+        &__title {
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 20px;
+          margin-top: 8px;
+          @include media-breakpoint-up(md) {
+            margin: 0 0 0 16px;
+            font-size: 16px;
+            line-height: 150%;
+          }
+        }
+        &__image {
+          @include media-breakpoint-up(md) {
+            width: 135px;
+            height: 0;
+            padding-top: 76px;
+          }
+        }
+      }
+      &__first {
+        .linked-item {
+          &__title {
+            @include media-breakpoint-up(md) {
+              font-weight: 600;
+              font-size: 18px;
+              line-height: 25px;
+              padding: 16px;
+              margin: 0;
+            }
+          }
+          &__image {
+            @include media-breakpoint-up(md) {
+              width: 100%;
+              height: 184px;
+            }
+          }
+        }
+      }
+
+      &__category-link {
+        font-weight: 600;
+        font-size: 14px;
+        line-height: 20px;
+        right: 16px;
+        bottom: 18px;
+        @include media-breakpoint-up(md) {
+          font-weight: 400;
+          font-size: 13px;
+          line-height: 18px;
+        }
+      }
+
+      @each $name, $color in $categories {
+        &.video_#{$name} {
+          .video-category__heading {
+            color: $color;
+          }
+          .video-category__category-link {
+            color: $color;
+            &::after {
+              border-color: transparent transparent transparent $color;
+            }
+            @include media-breakpoint-up(md) {
+              color: white;
+              &::after {
+                border-color: transparent transparent transparent white;
+              }
+            }
+          }
+        }
+      }
     }
   }
 }
