4ecbf0e965017e7f8ac6ebe7e1b0fafe70ee984b
DianYangFu <dianyangfu@gmail.com>
2022-04-12 15:32:21 +0800

    refactor(index): remove unneeded code for a/b test

--- experiments/index.js
+++ experiments/index.js
@@ -5,12 +5,10 @@
 
 import normalPostRelatedRedesign from './normal-post-related-redesign'
 import homepageLatestRedesign from './homepage-latest-redesign'
-import homepageEditorChoiceMobileRedesign from './homepage-editor-choice-mobile-redesign'
 import premiumListRelatedRedesign from './premium-list-related-redesign'
 
 export default [
   normalPostRelatedRedesign,
   homepageLatestRedesign,
-  homepageEditorChoiceMobileRedesign,
   premiumListRelatedRedesign,
 ]
