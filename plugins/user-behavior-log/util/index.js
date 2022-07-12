d452694bb3ca43dea48bc6109e55f98fa25c203c
Dian-Yang Fu <85677992+dyfu95@users.noreply.github.com>
2022-07-12 11:59:48 +0800

    chore(user-behavior-log): add code for testing `sendBeacon` (#711)

--- plugins/user-behavior-log/util/index.js
+++ plugins/user-behavior-log/util/index.js
@@ -52,10 +52,17 @@ export function sendLog(log) {
   if (_.isEmpty(log)) {
     return
   }
-  const blob = new Blob([JSON.stringify({ clientInfo: log })], {
+  const logJson = JSON.stringify({ clientInfo: log })
+  const testJson = JSON.stringify({ test: 123 })
+  console.log(typeof logJson)
+  const blob = new Blob([logJson], {
+    type: 'application/json; charset=UTF-8',
+  })
+  const testBlob = new Blob([testJson], {
     type: 'application/json; charset=UTF-8',
   })
   navigator.sendBeacon(`/${API_PATH_FRONTEND}/tracking`, blob)
+  navigator.sendBeacon(`/${API_PATH_FRONTEND}/tracking`, testBlob)
 }
 
 export function isScrollToBottom() {
