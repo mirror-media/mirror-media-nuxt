fb5e62047c86d1efe737b01f9116f9d111d584a4
Tsuki Akiba <erase2004@gmail.com>
2022-07-19 20:55:03 +0800

    docs: update function explanation comments

--- api/member-subscription-proxy-v2.js
+++ api/member-subscription-proxy-v2.js
@@ -1,5 +1,9 @@
 /*
- * This api endpoint might replace v0 in the future.
+ * This api endpoint is served as a proxy between front-end page and Israfel GraphQL api endpoint.
+ * This api is built with request authenication by verifing header of request.
+ * GraphQL request related to member list use this endpoint is recommanded.
+ * Customized query or mutation in apigateway should migrat to this endpoint in the future.
+ * This endpoint might replace v0 in the future.
  * The adventage of this endpoint is removing dependacies on apigateway.
  */
 
