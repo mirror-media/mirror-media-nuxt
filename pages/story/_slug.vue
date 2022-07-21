91fba276b55a87f1bdf29e14767a7f6be8fc2921
Dian-Yang Fu <85677992+dyfu95@users.noreply.github.com>
2022-07-21 18:47:10 +0800

    fix(story, pre/story): use `trim()` to remove space on `this.story.redirect` (#734)

--- pages/story/_slug.vue
+++ pages/story/_slug.vue
@@ -511,13 +511,14 @@ export default {
   async beforeMount() {
     await handleStoryPremiumRedirect(this.$nuxt.context)
     if (this.story.redirect && this.story.redirect?.trim()) {
+      const redirectHref = this.story.redirect?.trim()
       if (
-        this.story.redirect?.startsWith('https://') ||
-        this.story.redirect?.startsWith('http://')
+        redirectHref?.startsWith('https://') ||
+        redirectHref?.startsWith('http://')
       ) {
-        window.location.replace(this.story.redirect)
+        window.location.replace(redirectHref)
       } else {
-        window.location.replace(`/story/${this.story.redirect}`)
+        window.location.replace(`/story/${redirectHref}`)
       }
     }
     const { style, slug } = this.story
