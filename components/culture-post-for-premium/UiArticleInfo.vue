62ed2e64c9ce1c4283628a23356cd50add7b32bf
DianYangFu <dianyangfu@gmail.com>
2023-02-10 15:40:26 +0800

    feat: add donate button on premuim and pre/story page

--- components/culture-post-for-premium/UiArticleInfo.vue
+++ components/culture-post-for-premium/UiArticleInfo.vue
@@ -1,63 +1,66 @@
 <template>
-  <div class="article-info">
-    <div class="article-info__info info">
-      <div class="info__time time">
-        <p>發布時間 {{ publishTimeFormat }}</p>
-        <p class="time__update-time">更新時間 {{ updateTimeFormat }}</p>
-      </div>
-      <div
-        v-if="writers.length || photographers.length || extendByline"
-        class="info__people people"
-      >
-        <p v-if="writers.length" class="people__writers">
-          <span>記者｜</span>
-          <UiLink
-            v-for="writer in writers"
-            :key="writer.id"
-            class="link"
-            :href="`/author/${writer.id}`"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            {{ writer.name }}
-          </UiLink>
-        </p>
-        <p v-show="extendByline" class="people__extendByline">
-          <span>{{ extendByline }}</span>
-        </p>
-        <p v-if="photographers.length" class="people__photographers">
-          <span>攝影｜</span>
-          <UiLink
-            v-for="photographer in photographers"
-            :key="photographer.id"
-            class="link"
-            :href="`/author/${photographer.id}`"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            {{ photographer.name }}
-          </UiLink>
-        </p>
-      </div>
-      <div v-if="tags.length" class="info__tags tags">
-        <UiTag
-          v-for="tag in tags"
-          :key="tag.id"
-          class="tags__tag tag"
-          :href="`/tag/${tag.id}`"
-          target="_blank"
-          rel="noreferrer noopener"
+  <div class="article-info-and-donate">
+    <div class="article-info">
+      <div class="article-info__info info">
+        <div class="info__time time">
+          <p>發布時間 {{ publishTimeFormat }}</p>
+          <p class="time__update-time">更新時間 {{ updateTimeFormat }}</p>
+        </div>
+        <div
+          v-if="writers.length || photographers.length || extendByline"
+          class="info__people people"
         >
-          {{ tag.name }}
-        </UiTag>
+          <p v-if="writers.length" class="people__writers">
+            <span>記者｜</span>
+            <UiLink
+              v-for="writer in writers"
+              :key="writer.id"
+              class="link"
+              :href="`/author/${writer.id}`"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              {{ writer.name }}
+            </UiLink>
+          </p>
+          <p v-show="extendByline" class="people__extendByline">
+            <span>{{ extendByline }}</span>
+          </p>
+          <p v-if="photographers.length" class="people__photographers">
+            <span>攝影｜</span>
+            <UiLink
+              v-for="photographer in photographers"
+              :key="photographer.id"
+              class="link"
+              :href="`/author/${photographer.id}`"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              {{ photographer.name }}
+            </UiLink>
+          </p>
+        </div>
+        <div v-if="tags.length" class="info__tags tags">
+          <UiTag
+            v-for="tag in tags"
+            :key="tag.id"
+            class="tags__tag tag"
+            :href="`/tag/${tag.id}`"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            {{ tag.name }}
+          </UiTag>
+        </div>
       </div>
+      <ClientOnly>
+        <UiShareLinksHasCopyLink
+          v-observe-visibility="handleShareLinksVisibilityChanged"
+          class="article-info__share-links-wrapper"
+        />
+      </ClientOnly>
     </div>
-    <ClientOnly>
-      <UiShareLinksHasCopyLink
-        v-observe-visibility="handleShareLinksVisibilityChanged"
-        class="article-info__share-links-wrapper"
-      />
-    </ClientOnly>
+    <UiDonateButton v-if="$config.donateFeatureToggle" class="donate" />
   </div>
 </template>
 
@@ -65,7 +68,7 @@
 import dayjs from 'dayjs'
 import UiLink from '~/components/culture-post-for-premium/UiLink.vue'
 import UiTag from '~/components/culture-post-for-premium/UiTag.vue'
-
+import UiDonateButton from '~/components/UiDonateButton.vue'
 import UiShareLinksHasCopyLink from '~/components/UiShareLinksHasCopyLink.vue'
 
 export default {
@@ -73,6 +76,7 @@ export default {
     UiShareLinksHasCopyLink,
     UiTag,
     UiLink,
+    UiDonateButton,
   },
   props: {
     publishTime: {
@@ -121,8 +125,6 @@ export default {
 
 <style lang="scss" scoped>
 .article-info {
-  border-left: 2px solid #054f77;
-  padding: 4px 0 4px 24px;
   font-size: 14px;
   line-height: 1;
   color: rgba(0, 0, 0, 0.5);
@@ -176,5 +178,17 @@ export default {
       margin: 0 4px;
     }
   }
+}
+.donate {
+  margin-top: 20px;
+  @include media-breakpoint-up(md) {
+    margin-top: 24px;
+  }
+}
+.article-info-and-donate {
+  display: flex;
+  flex-direction: column;
+  border-left: 2px solid #054f77;
+  padding: 4px 0 4px 24px;
 }
 </style>
