8466512e0b072545ce64283d91396c776c031ff1
YiHsuan <v61265@gmail.com>
2023-03-29 16:12:07 +0800

    refactor(parner): not filter partner.public if feature toggle on

--- store/partners.js
+++ store/partners.js
@@ -1,9 +1,13 @@
 export const state = () => ({
   data: {},
+  isWarnlifeFeatureToggleOn: false,
 })
 
 export const getters = {
   displayedPartners(state) {
+    if (state.isWarnlifeFeatureToggleOn) {
+      return state.data.items
+    }
     return state.data.items?.filter((partner) => partner.public) ?? []
   },
 }
@@ -12,13 +16,18 @@ export const mutations = {
   setPartnersData(state, data) {
     state.data = data
   },
+  setIsWarnlifeFeaturToggleOn(state, isWarnlifeFeatureToggleOn) {
+    state.isWarnlifeFeatureToggleOn = isWarnlifeFeatureToggleOn
+  },
 }
 
 export const actions = {
   async fetchPartnersData({ commit }) {
+    const featureToggle = this.app.$config.warmlifeFeatureToggle
     const response =
       (await this.$fetchPartners({ maxResults: 25, page: 1 })) || {}
 
     commit('setPartnersData', response)
+    commit('setIsWarnlifeFeaturToggleOn', featureToggle)
   },
 }
