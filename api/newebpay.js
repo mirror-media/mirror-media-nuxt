28a03903d45704fcb719e0654e2e0897a621af05
YiHsuan <v61265@gmail.com>
2025-07-10 23:37:35 +0800

    fix(api): Correct NewebPay error handling and enable validation

--- api/newebpay.js
+++ api/newebpay.js
@@ -1,4 +1,4 @@
-import errors from '@twreporter/errors'
+// import errors from '@twreporter/errors'
 const NewebPay = require('@mirrormedia/newebpay-node')
 const { NEWEBPAY_KEY, NEWEBPAY_IV } = require('../configs/config')
 
@@ -9,68 +9,33 @@ module.exports = async function (req, res) {
      * 防止使用者自行修改 Amt 的值
      * 詳見：https://app.asana.com/1/614399484723017/project/1210077071799813/task/1210384428427743?focus=true
      */
-    /*
-     * let totalPrice = 0
-     * const { frequency } = tradeInfo
-     * switch (frequency) {
-     *   case 'one_time':
-     *     totalPrice = 10
-     *     break
-     *   case 'monthly':
-     *     totalPrice = 99
-     *     break
-     *   case 'yearly':
-     *     totalPrice = 799
-     *     break
-     *   default:
-     * }
-     */
 
-    /*
-     * if (totalPrice !== parseInt(tradeInfo.Amt)) {
-     *   throw new Error(
-     *     `Amt is not correct input, it is ${tradeInfo.Amt} but should be ${totalPrice}`
-     *   )
-     * }
-     */
+    let totalPrice = 0
+    const { frequency } = tradeInfo
+    switch (frequency) {
+      case 'one_time':
+        totalPrice = 10
+        break
+      case 'monthly':
+        totalPrice = 99
+        break
+      case 'yearly':
+        totalPrice = 799
+        break
+      default:
+    }
+
+    if (totalPrice !== parseInt(tradeInfo.Amt)) {
+      throw new Error(
+        `Amt is not correct input, it is ${tradeInfo.Amt} but should be ${totalPrice}`
+      )
+    }
 
     const newebpay = new NewebPay(NEWEBPAY_KEY, NEWEBPAY_IV)
     const encryptPostData = await newebpay.getEncryptedFormPostData(tradeInfo)
 
     res.send(encryptPostData)
   } catch (e) {
-    const annotatingError = errors.helpers.wrap(
-      e.message,
-      'UnhandledError',
-      'Error occurs while submit papermag'
-    )
-    console.log(
-      JSON.stringify({
-        severity: 'ERROR',
-        message: errors.helpers.printAll(
-          annotatingError,
-          {
-            withStack: true,
-            withPayload: true,
-          },
-          0,
-          0
-        ),
-      })
-    )
-
-    /*
-     * console.error(
-     *   JSON.stringify({
-     *     message: `papermag payload:`,
-     *     debugPayload: {
-     *       'req.body': req.body,
-     *       error: e.message, // Print the whole error object
-     *     },
-     *     'logging.googleapis.com/trace': `projects/mirrormedia-1470651750304/traces/papermag`,
-     *   })
-     * )
-     */
     res.status(500).send({
       status: 'error',
       message: e.message,
